66f350c043b59953d9a56c0f15c5024b2b667bb1
David Schmitt <david@dasz.at>
2008-07-16 12:26:40 +0000

    fix GetPrivateFieldValue to properly climb the object hierarchy
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@305 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2

--- Kistl.API/Helper.cs
+++ Kistl.API/Helper.cs
@@ -175,7 +175,13 @@ namespace Kistl.API
         public static T GetPrivateFieldValue<T>(this object obj, string propName)
         {
             if (obj == null) throw new ArgumentNullException("obj");
-            FieldInfo fi = obj.GetType().GetField(propName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            Type t = obj.GetType();
+            FieldInfo fi = null;
+            while (fi == null && t != null)
+            {
+                fi = t.GetField(propName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+                t = t.BaseType;
+            }
             if (fi == null) throw new ArgumentOutOfRangeException("propName", string.Format("Field {0} was not found in Type {1}", propName, obj.GetType().FullName));
             return (T)fi.GetValue(obj);
         }
