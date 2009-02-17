c1be9fe5c1030fc033e08b54e7791de88ec8ff93
David Schmitt <david@dasz.at>
2009-02-17 09:47:37 +0000

    Create and Test a SerializerMock for the TestObjClass
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@639 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2

--- Tests/Kistl.API.Server.Tests/TestObjClass.cs
+++ Tests/Kistl.API.Server.Tests/TestObjClass.cs
@@ -1,14 +1,9 @@
 using System;
 using System.Collections.Generic;
+
 namespace Kistl.API.Server.Tests
 {
-    public interface TestObjClass : IDataObject
+    public interface TestObjClass : Kistl.API.Mocks.TestObjClass<TestObjClass, int>
     {
-        TestObjClass BaseTestObjClass { get; set; }
-        string StringProp { get; set; }
-        ICollection<TestObjClass> SubClasses { get; }
-        int TestEnumProp { get; set; }
-        void TestMethod(DateTime DateTimeParamForTestMethod);
-        ICollection<string> TestNames { get; }
     }
 }
---
+++ Tests/Kistl.API.Tests/Tests/SerializerMockTests.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+using Kistl.API;
+using Kistl.API.Mocks;
+
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using NUnit.Framework.SyntaxHelpers;
+
+
+namespace Kistl.API.Tests
+{
+    [TestFixture]
+    public class SerializerMockTests
+    {
+        MemoryStream ms;
+        BinaryWriter sw;
+        BinaryReader sr;
+
+        interface LocalMock : TestObjClass<LocalMock, TestEnum> { }
+
+        [SetUp]
+        public void SetUp()
+        {
+            ms = new MemoryStream();
+            sw = new BinaryWriter(ms);
+            sr = new BinaryReader(ms);
+        }
+        
+        [Test]
+        public void AssertCorrectContents_correctly_identifies_ToStream_result()
+        {
+            TestObjClassSerializationMock.ToStream<LocalMock, TestEnum>(sw);
+            ms.Seek(0, SeekOrigin.Begin);
+            TestObjClassSerializationMock.AssertCorrectContents<LocalMock, TestEnum>(sr);
+            Assert.That(ms.Position, Is.EqualTo(ms.Length), "AssertCorrectContents didn't read complete stream");
+        }
+	
+    }
+}
