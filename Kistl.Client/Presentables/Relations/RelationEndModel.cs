c2108468b2c5504cf06128390402c115024ccfd2
David Schmitt <david@dasz.at>
2009-03-26 15:31:46 +0000

    Interfaces for Relation(End)Model
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@778 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2

---
+++ Kistl.Client/Presentables/Relations/RelationEndModel.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Kistl.API;
+using Kistl.App.Base;
+
+namespace Kistl.Client.Presentables.Relations
+{
+    public class RelationEndModel
+        : DataObjectModel
+    {
+        private RelationEnd _relationEnd;
+
+        public RelationEndModel(
+            IGuiApplicationContext appCtx, IKistlContext dataCtx,
+            RelationEnd relEnd)
+            : base(appCtx, dataCtx, relEnd)
+        {
+            _relationEnd = relEnd;
+        }
+
+        #region Public interface
+
+        public PropertyModel<string> RoleName { get; private set; }
+
+        public DataObjectModel Navigator { get; private set; }
+
+        public ICommand CreateNavigatorCommand { get; private set; }
+
+        public ObjectReferenceModel RelatedClass { get; private set; }
+
+        public PropertyModel<bool> HasOrderPersisted { get; private set; }
+
+        #endregion
+
+        #region Async handlers and UI callbacks
+        #endregion
+
+        #region Event handlers
+        #endregion
+
+    }
+}
---
+++ Kistl.Client/Presentables/Relations/RelationModel.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Kistl.API;
+using Kistl.App.Base;
+
+namespace Kistl.Client.Presentables.Relations
+{
+
+    public class RelationModel
+        : DataObjectModel
+    {
+        private Relation _relation;
+
+        public RelationModel(
+            IGuiApplicationContext appCtx, IKistlContext dataCtx,
+            Relation rel)
+            : base(appCtx, dataCtx, rel)
+        {
+            _relation = rel;
+        }
+
+        #region Public interface
+
+        public RelationEndModel A { get; private set; }
+
+        public RelationEndModel B { get; private set; }
+
+        public PropertyModel<string> Description { get; private set; }
+
+        #endregion
+
+        #region Async handlers and UI callbacks
+        #endregion
+
+        #region Event handlers
+        #endregion
+
+    }
+
+}
