fb8c6808c2cb94b9e169c96169636ab51a6754ec
David Schmitt <david@dasz.at>
2009-04-21 08:45:54 +0000

    Convert Test to Theory and try it on the DB as well
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@869 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2

--- Tests/Kistl.IntegrationTests/Tests/TypeRefs/should_convert_types_correctly.cs
+++ Tests/Kistl.IntegrationTests/Tests/TypeRefs/should_convert_types_correctly.cs
@@ -2,29 +2,38 @@ using System;
 using System.Collections.Generic;
 
 using Kistl.API;
+using Kistl.API.Client;
 using Kistl.App.Extensions;
 
 using NUnit.Framework;
 
 namespace Kistl.IntegrationTests.TypeRefs
 {
-	[TestFixture]
-	public class should_convert_types_correctly
-	{
-		
-		// These are important TypeRefs for the GUI which must exist
-		// using int for "struct" types, string for "class" types
-		[TestCase(typeof(int))]
-		[TestCase(typeof(int?))]
-		[TestCase(typeof(string))]
-		[TestCase(typeof(ICollection<int>))]
-		[TestCase(typeof(ICollection<int?>))]
-		[TestCase(typeof(ICollection<string>))]
-		public void when_calling_ToRef_on_a_Type(Type systemType)
-		{
-			var tr = systemType.ToRef(FrozenContext.Single);
-			Assert.That(tr, Is.Not.Null);
-			Assert.That(tr.AsType(true), Is.EqualTo(systemType));
-		}
-	}
+    [TestFixture]
+    public class should_convert_types_correctly
+    {
+        // These are important TypeRefs for the GUI which must exist
+        // using int for "struct" types, string for "class" types
+        [Datapoints]
+        public static Type[] TestTypes = new[] { typeof(int), typeof(int?), typeof(string), typeof(ICollection<int>), typeof(ICollection<int?>), typeof(ICollection<string>) };
+
+        [Theory]
+        public void when_calling_ToFrozenRef_on_a_Type(Type systemType)
+        {
+            var tr = systemType.ToRef(FrozenContext.Single);
+            Assert.That(tr, Is.Not.Null);
+            Assert.That(tr.AsType(true), Is.EqualTo(systemType));
+        }
+
+        [Theory]
+        public void when_calling_ToRef_on_a_Type(Type systemType)
+        {
+            using (var ctx = KistlContext.GetContext())
+            {
+                var tr = systemType.ToRef(ctx);
+                Assert.That(tr, Is.Not.Null);
+                Assert.That(tr.AsType(true), Is.EqualTo(systemType));
+            }
+        }
+    }
 }
