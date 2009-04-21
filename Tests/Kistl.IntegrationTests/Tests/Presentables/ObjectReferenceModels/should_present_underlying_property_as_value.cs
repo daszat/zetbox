6bf266df6096665685a0ba583b3b63d3ad41f0d8
David Schmitt <david@dasz.at>
2009-04-21 09:25:09 +0000

    create first integration test to test Presentables
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@870 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2

---
+++ Tests/Kistl.IntegrationTests/Tests/Presentables/ObjectReferenceModels/should_present_underlying_property_as_value.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Linq;
+
+using Kistl.API.Client;
+using Kistl.App.Base;
+using Kistl.Client;
+using Kistl.Client.Presentables;
+
+using NUnit.Framework;
+
+namespace Kistl.IntegrationTests.Presentables.ObjectReferenceModels
+{
+	[TestFixture]
+	public class should_present_underlying_property_as_value
+	{
+		[Test]
+		public void when_displaying_a_value()
+		{
+			using(var ctx = KistlContext.GetContext())
+			{
+				var obj = ctx.GetQuery<TypeRef>().First();
+				var typeRefClass = ctx.GetQuery<ObjectClass>()
+					.Single(oc => oc.ClassName == "TypeRef");
+				var assemblyProperty = ctx.GetQuery<ObjectReferenceProperty>()
+					.Single(p => p.ObjectClass.ID == typeRefClass.ID && p.PropertyName == "Assembly");
+				var orm = new ObjectReferenceModel(
+					GuiApplicationContext.Current, ctx,
+					obj, assemblyProperty);
+				
+				Assert.That(orm.Value.ID , Is.EqualTo(obj.Assembly.ID));
+			}
+		}
+	}
+}
