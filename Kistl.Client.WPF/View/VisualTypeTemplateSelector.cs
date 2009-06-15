b4550bb1664284640f54e8aede565bd42359ae1a
David Schmitt <david@dasz.at>
2009-06-15 08:31:26 +0000

    Implement ReadOnly property.
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@967 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2

--- Kistl.Client.WPF/View/VisualTypeTemplateSelector.cs
+++ Kistl.Client.WPF/View/VisualTypeTemplateSelector.cs
@@ -12,37 +12,75 @@ namespace Kistl.Client.WPF.View
     using Kistl.App.Base;
     using Kistl.App.Extensions;
     using Kistl.App.GUI;
+    using Kistl.Client.GUI;
     using Kistl.Client.Presentables;
 
+    /// <summary>
+    /// A <see cref="DataTemplateSelector"/> to choose the appropriate
+    /// <see cref="IView"/> for a specified <see cref="PresentableModel"/>.
+    /// </summary>
     public class VisualTypeTemplateSelector
         : DataTemplateSelector
     {
-        public static DataTemplate SelectTemplate(PresentableModel mdl, VisualType? vType)
+        /// <summary>
+        /// The core method of this class. Chooses the appropriate 
+        /// <see cref="IView"/> for a specified <see cref="PresentableModel"/> 
+        /// according to the specified parameters.
+        /// </summary>
+        /// <param name="mdl">the model to display</param>
+        /// <param name="visualType">which kind of view to use</param>
+        /// <param name="readOnly">whether or not the view should be read-only</param>
+        /// <returns>a DataTemplate capable of displaying the specified model</returns>
+        public static DataTemplate SelectTemplate(PresentableModel mdl, VisualType? visualType, bool readOnly)
         {
             PresentableModelDescriptor pmd = mdl.GetType().ToRef(FrozenContext.Single).GetPresentableModelDescriptor();
 
-            var vDesc = vType.HasValue ? pmd.GetViewDescriptor(Toolkit.WPF, vType.Value) : pmd.GetDefaultViewDescriptor(Toolkit.WPF);
+            var visualDesc = visualType.HasValue 
+                ? pmd.GetViewDescriptor(Toolkit.WPF, visualType.Value, readOnly) 
+                : pmd.GetDefaultViewDescriptor(Toolkit.WPF, readOnly);
 
             DataTemplate result = new DataTemplate();
-            if (vDesc != null)
+            if (visualDesc != null)
             {
-                result.VisualTree = new FrameworkElementFactory(vDesc.ControlRef.AsType(true));
+                result.VisualTree = new FrameworkElementFactory(visualDesc.ControlRef.AsType(true));
             }
             return result;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the VisualTypeTemplateSelector class.
+        /// </summary>
+        public VisualTypeTemplateSelector()
+        {
+            RequestedType = null;
+            ReadOnly = false;
+        }
+
+        /// <summary>
+        /// Gets or sets the kind of view to use. To select the default view, leave this property at its default null value.
+        /// </summary>
         public VisualType? RequestedType
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether read-only views should be preferred.
+        /// </summary>
+        public bool ReadOnly
+        {
+            get;
+            set;
+        }
+
+        /// <inheritdoc/>
         public override DataTemplate SelectTemplate(object item, DependencyObject container)
         {
             var model = item as PresentableModel;
             if (model != null)
             {
-                return VisualTypeTemplateSelector.SelectTemplate(model, this.RequestedType);
+                return VisualTypeTemplateSelector.SelectTemplate(model, this.RequestedType, ReadOnly);
             }
             else
             {
