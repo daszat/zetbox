f358e239c4b3e31e58fedba1d73da950f600eaec
David Schmitt <david@dasz.at>
2009-07-07 16:00:21 +0000

    remove "pause" from end of cmd file to allow usage in CCNet without(!) timeouts
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@1004 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2

--- DataImport.cmd
+++ DataImport.cmd
@@ -3,4 +3,3 @@ cd Kistl.Server
 osql -S .\sqlexpress -E -d Kistl -i Database\Scripts\DropTables.sql
 bin\debug\Kistl.Server.exe -updateschema Database\Database.xml -import Database\Database.xml -checkschema 
 bin\debug\Kistl.Server.exe -generate
-pause
