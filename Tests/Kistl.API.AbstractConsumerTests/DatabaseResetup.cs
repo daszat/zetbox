9b0ab53495bbab1e2ee1bec5247d4b3ef117b215
David Schmitt <david@dasz.at>
2009-07-07 17:39:00 +0000

    Better fix the DatabaseResetup utility
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@1009 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2

--- Tests/Kistl.API.AbstractConsumerTests/DatabaseResetup.cs
+++ Tests/Kistl.API.AbstractConsumerTests/DatabaseResetup.cs
@@ -23,7 +23,14 @@ namespace Kistl.API.AbstractConsumerTests
 
         protected void ResetDatabase(KistlConfig config)
         {
-            // we now depend on the DataImport.cmd to create a proper database for testing
+            string basePath = @"..\..\..\..";
+            Console.WriteLine("Killing Database");
+            Process.Start("osql", String.Format(@"osql -S .\sqlexpress -E -d Kistl -i {0}\Kistl.Server\Database\Scripts\DropTables.sql", basePath)).WaitForExit();
+            Console.WriteLine("Filling Database");
+            Console.WriteLine(Environment.CurrentDirectory);
+            Process.Start(basePath + @"\bin\Debug\Kistl.Server.exe", String.Format(@"{0} -updateschema {1}\Kistl.Server\Database\Database.xml -import {1}\Kistl.Server\Database\Database.xml -checkschema", config.ConfigFilePath, basePath)).WaitForExit();
+            Console.WriteLine("Generate Source");
+            Process.Start(basePath + @"\bin\Debug\Kistl.Server.exe", @"-generate").WaitForExit();
         }
 
     }
