9ebc07e85f1ae391d4fc14a76a28682b345c81e2
David Schmitt <david@dasz.at>
2009-07-07 18:29:33 +0000

    Remove useless directives from Kistl.Server.csproj; use proper working directory in DatabaseResetup
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@1011 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2

--- Tests/Kistl.API.AbstractConsumerTests/DatabaseResetup.cs
+++ Tests/Kistl.API.AbstractConsumerTests/DatabaseResetup.cs
@@ -18,17 +18,14 @@ namespace Kistl.API.AbstractConsumerTests
     /// </summary>
     public abstract class DatabaseResetup
     {
-        // TODO: don't hardcode db script path here
-        readonly static string CurrentDbScript = @"Kistl.Server.Database.Database.sql";
-
         protected void ResetDatabase(KistlConfig config)
         {
-            string basePath = @"..\..\..\..";
+            string basePath = @"..\..\..\..\Kistl.Server";
             Console.WriteLine("Killing Database");
-            Process.Start("osql", String.Format(@"osql -S .\sqlexpress -E -d Kistl -i {0}\Kistl.Server\Database\Scripts\DropTables.sql", basePath)).WaitForExit();
+            Process.Start("osql", String.Format(@"osql -S .\sqlexpress -E -d Kistl -i {0}\Database\Scripts\DropTables.sql", basePath)).WaitForExit();
             Console.WriteLine("Filling Database");
             Console.WriteLine(Environment.CurrentDirectory);
-            Process.Start(basePath + @"\bin\Debug\Kistl.Server.exe", String.Format(@"{0} -updateschema {1}\Kistl.Server\Database\Database.xml -import {1}\Kistl.Server\Database\Database.xml -checkschema", config.ConfigFilePath, basePath)).WaitForExit();
+            Process.Start(basePath + @"\bin\Debug\Kistl.Server.exe", String.Format(@"{0} -updateschema {1}\Database\Database.xml -import {1}\Database\Database.xml -checkschema", config.ConfigFilePath, basePath)).WaitForExit();
             Console.WriteLine("Generate Source");
             Process.Start(basePath + @"\bin\Debug\Kistl.Server.exe", @"-generate").WaitForExit();
         }
