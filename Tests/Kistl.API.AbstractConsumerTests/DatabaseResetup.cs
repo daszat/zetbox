313c9a038f751a5ed7eb9405a403367cf5bade3c
David Schmitt <david@dasz.at>
2009-07-07 18:53:33 +0000

    Again fix DatabaseResetup, this time use the old SQL code and the new DropTables.sql to clear the DB manually instead of using osql
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@1012 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2

--- Tests/Kistl.API.AbstractConsumerTests/DatabaseResetup.cs
+++ Tests/Kistl.API.AbstractConsumerTests/DatabaseResetup.cs
@@ -22,12 +22,44 @@ namespace Kistl.API.AbstractConsumerTests
         {
             string basePath = @"..\..\..\..\Kistl.Server";
             Console.WriteLine("Killing Database");
-            Process.Start("osql", String.Format(@"osql -S .\sqlexpress -E -d Kistl -i {0}\Database\Scripts\DropTables.sql", basePath)).WaitForExit();
+            Assert.That(config.Server.ConnectionString, Text.Contains("_test"), "test databases should be marked with '_test' in the connection string");
+
+            try
+            {
+                Trace.TraceInformation("Resetting Database");
+                using (var db = new SqlConnection(config.Server.ConnectionString))
+                {
+                    db.Open();
+                    var scriptStream = new StreamReader(typeof(Kistl.Server.Helper).Assembly.GetManifestResourceStream("Kistl.Server.Database.Scripts.DropTables.sql"));
+                    var databaseScript = scriptStream.ReadToEnd();
+                    using (var tx = db.BeginTransaction())
+                    {
+                        foreach (var cmdString in Regex.Split(databaseScript, "\r?\nGO\r?\n").Where(s => !String.IsNullOrEmpty(s)))
+                        {
+                            using (var cmd = new SqlCommand(cmdString, db, tx))
+                            {
+                                cmd.ExecuteNonQuery();
+                            }
+                        }
+                        tx.Commit();
+                    }
+                }
+                Trace.TraceInformation("Done Resetting Database");
+            }
+            catch (Exception error)
+            {
+                Trace.TraceError("Error ({0}) while resetting database: {1}", error.GetType().Name, error.Message);
+                Trace.TraceError(error.ToString());
+                Trace.TraceError(error.StackTrace);
+
+                throw error;
+            }
+
             Console.WriteLine("Filling Database");
             Console.WriteLine(Environment.CurrentDirectory);
             Process.Start(basePath + @"\bin\Debug\Kistl.Server.exe", String.Format(@"{0} -updateschema {1}\Database\Database.xml -import {1}\Database\Database.xml -checkschema", config.ConfigFilePath, basePath)).WaitForExit();
             Console.WriteLine("Generate Source");
-            Process.Start(basePath + @"\bin\Debug\Kistl.Server.exe", @"-generate").WaitForExit();
+            Process.Start(basePath + @"\bin\Debug\Kistl.Server.exe", String.Format(@"{0} -generate", config.ConfigFilePath)).WaitForExit();
         }
 
     }
