4a524a9a9eeb9b36e4745fc2ac22e0783dfa6823
David Schmitt <david@dasz.at>
2009-07-28 13:08:39 +0000

    One stop shop for resetting the database
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@1055 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2

---
+++ !FullReset.cmd
@@ -0,0 +1,36 @@
+rem @echo off
+
+rem remove old assemblies
+del /S /Q bin\ C:\temp\KistlCodeGen\*
+IF ERRORLEVEL 1 GOTO FAIL
+C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe /m Kistl.Complete.sln /target:Clean
+IF ERRORLEVEL 1 GOTO FAIL
+
+rem drop database contents
+osql -S .\sqlexpress -E -d Kistl -i Kistl.Server\Database\Scripts\DropTables.sql
+IF ERRORLEVEL 1 GOTO FAIL
+
+rem build bootstrapper
+C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe /m Kistl.Complete.sln
+IF ERRORLEVEL 1 GOTO FAIL
+
+cd Kistl.Server
+
+rem populate database and generate other assemblies
+bin\debug\Kistl.Server.exe -updateschema Database\Database.xml
+IF ERRORLEVEL 1 GOTO FAIL
+bin\debug\Kistl.Server.exe -deploy Database\Database.xml -updateschema -checkschema
+IF ERRORLEVEL 1 GOTO FAIL
+bin\debug\Kistl.Server.exe -generate
+IF ERRORLEVEL 1 GOTO FAIL
+
+cd ..
+
+rem refresh local code
+GetCodeGen.cmd
+GOTO EOF
+
+:FAIL
+echo Aborting reset.
+pause
+:EOF
