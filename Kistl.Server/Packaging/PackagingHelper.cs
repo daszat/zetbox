7f541adf258be998a467ae97871ad4155666b48a
David Schmitt <david@dasz.at>
2009-07-29 12:46:06 +0000

    Remove explicit namespace references
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@1065 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2

--- Kistl.Server/Packaging/PackagingHelper.cs
+++ Kistl.Server/Packaging/PackagingHelper.cs
@@ -9,6 +9,7 @@ namespace Kistl.Server.Packaging
 
     using Kistl.API;
     using Kistl.App.Base;
+    using Kistl.App.GUI;
 
     internal static class PackagingHelper
     {
@@ -17,50 +18,50 @@ namespace Kistl.Server.Packaging
             IList<IPersistenceObject> result = new List<IPersistenceObject>();
             int moduleID = module.ID;
 
-            AddMetaObjects(result, ctx.GetQuery<Kistl.App.Base.Module>().Where(i => i.ID == moduleID).OrderBy(m => m.ModuleName));
+            AddMetaObjects(result, ctx.GetQuery<Module>().Where(i => i.ID == moduleID).OrderBy(m => m.ModuleName));
 
-            AddMetaObjects(result, ctx.GetQuery<Kistl.App.Base.DataType>().Where(i => i.Module.ID == moduleID)
+            AddMetaObjects(result, ctx.GetQuery<DataType>().Where(i => i.Module.ID == moduleID)
                 .OrderBy(i => i.ClassName));
-            AddMetaObjects(result, ctx.GetPersistenceObjectQuery<Kistl.App.Base.ObjectClass_implements_Interface_RelationEntry>().Where(i => i.A.Module.ID == moduleID || i.B.Module.ID == moduleID)
+            AddMetaObjects(result, ctx.GetPersistenceObjectQuery<ObjectClass_implements_Interface_RelationEntry>().Where(i => i.A.Module.ID == moduleID || i.B.Module.ID == moduleID)
                 .OrderBy(i => i.A.ClassName).ThenBy(i => i.B.ClassName));
-            AddMetaObjects(result, ctx.GetQuery<Kistl.App.Base.Property>().Where(i => i.Module.ID == moduleID)
+            AddMetaObjects(result, ctx.GetQuery<Property>().Where(i => i.Module.ID == moduleID)
                 .OrderBy(i => i.ObjectClass.ClassName).ThenBy(i => i.PropertyName));
-            AddMetaObjects(result, ctx.GetQuery<Kistl.App.Base.Relation>().Where(i => i.Module.ID == moduleID)
+            AddMetaObjects(result, ctx.GetQuery<Relation>().Where(i => i.Module.ID == moduleID)
                 .OrderBy(i => i.A.Type.ClassName).ThenBy(i => i.Verb).ThenBy(i => i.B.Type.ClassName));
-            AddMetaObjects(result, ctx.GetQuery<Kistl.App.Base.RelationEnd>().Where(i => (i.AParent != null && i.AParent.Module.ID == moduleID) || (i.BParent != null && i.BParent.Module.ID == moduleID))
+            AddMetaObjects(result, ctx.GetQuery<RelationEnd>().Where(i => (i.AParent != null && i.AParent.Module.ID == moduleID) || (i.BParent != null && i.BParent.Module.ID == moduleID))
                 .OrderBy(i => i.Type.ClassName).ThenBy(i => i.RoleName).ThenBy(i => i.ExportGuid));
-            AddMetaObjects(result, ctx.GetQuery<Kistl.App.Base.EnumerationEntry>().Where(i => i.Enumeration.Module.ID == moduleID)
+            AddMetaObjects(result, ctx.GetQuery<EnumerationEntry>().Where(i => i.Enumeration.Module.ID == moduleID)
                 .OrderBy(i => i.Enumeration.ClassName).ThenBy(i => i.Name));
 
-            AddMetaObjects(result, ctx.GetQuery<Kistl.App.Base.Method>().Where(i => i.Module.ID == moduleID)
+            AddMetaObjects(result, ctx.GetQuery<Method>().Where(i => i.Module.ID == moduleID)
                 .OrderBy(i => i.ObjectClass.ClassName).ThenBy(i => i.MethodName));
-            AddMetaObjects(result, ctx.GetQuery<Kistl.App.Base.BaseParameter>().Where(i => i.Method.Module.ID == moduleID)
+            AddMetaObjects(result, ctx.GetQuery<BaseParameter>().Where(i => i.Method.Module.ID == moduleID)
                 .OrderBy(i => i.Method.ObjectClass.ClassName).ThenBy(i => i.Method.MethodName).ThenBy(i => i.ParameterName));
-            AddMetaObjects(result, ctx.GetQuery<Kistl.App.Base.MethodInvocation>().Where(i => i.Module.ID == moduleID)
+            AddMetaObjects(result, ctx.GetQuery<MethodInvocation>().Where(i => i.Module.ID == moduleID)
                 .OrderBy(i => i.InvokeOnObjectClass.ClassName).ThenBy(i => i.Method.MethodName).ThenBy(i => i.Implementor.FullName).ThenBy(i => i.MemberName));
-            AddMetaObjects(result, ctx.GetQuery<Kistl.App.Base.PropertyInvocation>().Where(i => i.InvokeOnProperty.Module.ID == moduleID)
+            AddMetaObjects(result, ctx.GetQuery<PropertyInvocation>().Where(i => i.InvokeOnProperty.Module.ID == moduleID)
                 .OrderBy(i => i.InvokeOnProperty.PropertyName).ThenBy(i => i.Implementor.FullName).ThenBy(i => i.MemberName));
 
             // TODO: Add Module to Constraint - or should that not be changable by other modules?
-            AddMetaObjects(result, ctx.GetQuery<Kistl.App.Base.Constraint>().Where(i => i.ConstrainedProperty.Module.ID == moduleID)
+            AddMetaObjects(result, ctx.GetQuery<Constraint>().Where(i => i.ConstrainedProperty.Module.ID == moduleID)
                 .OrderBy(i => i.ConstrainedProperty.ObjectClass.ClassName).ThenBy(i => i.ConstrainedProperty.PropertyName));
 
             // TODO: Add Module to DefaultPropertyValue - or should that not be changable by other modules?
-            AddMetaObjects(result, ctx.GetQuery<Kistl.App.Base.DefaultPropertyValue>().Where(i => i.Property.Module.ID == moduleID)
+            AddMetaObjects(result, ctx.GetQuery<DefaultPropertyValue>().Where(i => i.Property.Module.ID == moduleID)
                 .OrderBy(i => i.Property.ObjectClass.ClassName).ThenBy(i => i.Property.PropertyName));
 
-            AddMetaObjects(result, ctx.GetQuery<Kistl.App.Base.Assembly>().Where(i => i.Module.ID == moduleID)
+            AddMetaObjects(result, ctx.GetQuery<Assembly>().Where(i => i.Module.ID == moduleID)
                 .OrderBy(i => i.AssemblyName));
-            AddMetaObjects(result, ctx.GetQuery<Kistl.App.Base.TypeRef>().Where(i => i.Assembly.Module.ID == moduleID)
+            AddMetaObjects(result, ctx.GetQuery<TypeRef>().Where(i => i.Assembly.Module.ID == moduleID)
                 .OrderBy(i => i.Assembly.AssemblyName).ThenBy(i => i.FullName));
-            AddMetaObjects(result, ctx.GetPersistenceObjectQuery<Kistl.App.Base.TypeRef_hasGenericArguments_TypeRef_RelationEntry>().Where(i => i.A.Assembly.Module.ID == moduleID || i.B.Assembly.Module.ID == moduleID)
+            AddMetaObjects(result, ctx.GetPersistenceObjectQuery<TypeRef_hasGenericArguments_TypeRef_RelationEntry>().Where(i => i.A.Assembly.Module.ID == moduleID || i.B.Assembly.Module.ID == moduleID)
                 .OrderBy(i => i.A.Assembly.AssemblyName).ThenBy(i => i.B.Assembly.AssemblyName));
 
-            AddMetaObjects(result, ctx.GetQuery<Kistl.App.GUI.Icon>().Where(i => i.Module.ID == moduleID)
+            AddMetaObjects(result, ctx.GetQuery<Icon>().Where(i => i.Module.ID == moduleID)
                 .OrderBy(i => i.IconFile));
-            AddMetaObjects(result, ctx.GetQuery<Kistl.App.GUI.PresentableModelDescriptor>().Where(i => i.Module.ID == moduleID)
+            AddMetaObjects(result, ctx.GetQuery<PresentableModelDescriptor>().Where(i => i.Module.ID == moduleID)
                 .OrderBy(i => i.PresentableModelRef.Assembly.AssemblyName).ThenBy(i => i.PresentableModelRef.FullName));
-            AddMetaObjects(result, ctx.GetQuery<Kistl.App.GUI.ViewDescriptor>().Where(i => i.Module.ID == moduleID)
+            AddMetaObjects(result, ctx.GetQuery<ViewDescriptor>().Where(i => i.Module.ID == moduleID)
                 .OrderBy(i => i.ControlRef.Assembly.AssemblyName).ThenBy(i => i.ControlRef.FullName));
 
             return result;
