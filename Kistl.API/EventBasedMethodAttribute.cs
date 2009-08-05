c53cb0a3f88120206a80508c6af16b55434d31de
David Schmitt <david@dasz.at>
2009-08-05 15:33:25 +0000

    An attribute to ease the work of CustomActionsManagers
    Use this attribute to declare a Method as event based. On start-up a
    custom ActionsManager will use the meta data from the data store to
    connect such methods to their MethodInvocations by managing the named
    Event.
    
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@1105 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2

---
+++ Kistl.API/EventBasedMethodAttribute.cs
@@ -0,0 +1,41 @@
+
+namespace Kistl.API
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// Use this attribute to declare a Method as event based. On start-up a 
+    /// custom ActionsManager will use the meta data from the data store to 
+    /// connect such methods to their MethodInvocations by managing the named 
+    /// Event.
+    /// </summary>
+    [global::System.AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+    public sealed class EventBasedMethodAttribute
+        : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the EventBasedMethodAttribute class with a specified event name.
+        /// </summary>
+        /// <param name="eventName">the event name behind this method</param>
+        public EventBasedMethodAttribute(string eventName)
+        {
+            _eventName = eventName;
+        }
+
+        /// <summary>
+        /// Gets the name of the event that is raised when the Method is called.
+        /// </summary>
+        public string EventName
+        {
+            get { return _eventName; }
+        }
+
+        /// <summary>
+        /// Backing store for the EventName property.
+        /// </summary>
+        private readonly string _eventName;
+    }
+}
