0c9035f7e0514f3b950eb50244203556bbd7c0f4
Arthur Zaczek <arthur@dasz.at>
2009-12-02 14:33:39 +0000

    Fixes in PublishScript Removed RemotingLogging in server app.config
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@1224 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2

--- !PublishAllUI.cmd
+++ !PublishAllUI.cmd
@@ -1,6 +1,5 @@
 @echo off
-bin\Debug\Kistl.Server.Service.exe -generate -updateschema -repairschema
-bin\debug\Kistl.Server.Service.exe -publish Kistl.Server\Database\Database.xml *
-cd ..
+bin\Debug\Kistl.Server.Service.exe Kistl.Server.Service\DefaultConfig.xml -generate -updateschema -repairschema
+bin\debug\Kistl.Server.Service.exe Kistl.Server.Service\DefaultConfig.xml -publish Kistl.Server\Database\Database.xml *
 call GetCodeGen.cmd
 pause
