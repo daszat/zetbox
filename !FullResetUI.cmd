5efe73323d0989adbf1bba3eb95f0bd78745ad2b
David Schmitt <david@dasz.at>
2009-12-17 15:26:59 +0000

    Migrate FullReset contents into CCNet.msbuild and use parameters in CCNet.confi on SRV2008 and !FullResetUI.cmd to find the various tools and paths
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@1306 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2

--- !FullResetUI.cmd
+++ !FullResetUI.cmd
@@ -1,35 +1,10 @@
 @echo off
 
-rem remove old assemblies
-del /S /Q bin\
-IF ERRORLEVEL 1 GOTO FAIL
-C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe /m Kistl.Complete.sln /target:Clean
-IF ERRORLEVEL 1 GOTO FAIL
-
-rem drop database contents
-osql -S .\sqlexpress -E -d Kistl -i Kistl.Server\Database\Scripts\DropTables.sql
-IF ERRORLEVEL 1 GOTO FAIL
-
-rem build bootstrapper
-C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe /m Kistl.Complete.sln
-IF ERRORLEVEL 1 GOTO FAIL
-
-
-rem populate database and generate other assemblies
-bin\debug\bin\Server\Kistl.Server.Service.exe Kistl.Server.Service\DefaultConfig.xml -updateschema Kistl.Server\Database\Database.xml
-IF ERRORLEVEL 1 GOTO FAIL
-bin\debug\bin\Server\Kistl.Server.Service.exe Kistl.Server.Service\DefaultConfig.xml -deploy Kistl.Server\Database\Database.xml -updateschema -checkschema
-IF ERRORLEVEL 1 GOTO FAIL
-bin\debug\bin\Server\Kistl.Server.Service.exe Kistl.Server.Service\DefaultConfig.xml -generate
+C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe CCNet.msbuild /p:SourceLocation=P:\Kistl /p:ArebisMsBuildPath=P:\ArebisCGen\Arebis.CodeGeneration.MsBuild
 IF ERRORLEVEL 1 GOTO FAIL
 
 rem regenerate Database.xml to prove roundtrippability
 bin\debug\bin\Server\Kistl.Server.Service.exe Kistl.Server.Service\DefaultConfig.xml -publish Kistl.Server\Database\Database.xml *
-IF ERRORLEVEL 1 GOTO FAIL
-
-
-rem refresh local code
-call GetCodeGen.cmd
 IF ERRORLEVEL 1 GOTO FAIL
 
 echo ********************************************************************************
