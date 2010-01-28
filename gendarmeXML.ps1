14649da0c21a9a402398091854cb503777032f91
Arthur Zaczek <arthur@dasz.at>
2010-01-28 17:33:22 +0000

    Simplify gendarmeXML.ps1
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@1453 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2

--- gendarmeXML.ps1
+++ gendarmeXML.ps1
@@ -1,31 +1,14 @@
 param($SolutionDir)
 
-function Convert-WithXslt($originalXmlFilePath, $xslFilePath, $outputFilePath) 
+function Convert-WithXslt($xmlFilePath, $xsltFilePath, $outputFilePath) 
 {
-   ## Simplistic error handling
-   $xslFilePath = resolve-path $xslFilePath
-   if( -not (test-path $xslFilePath) ) { throw "Can't find the XSL file" } 
-   $originalXmlFilePath = resolve-path $originalXmlFilePath
-   if( -not (test-path $originalXmlFilePath) ) { throw "Can't find the XML file" } 
+   $xsltFilePath = resolve-path $xsltFilePath
+   $xmlFilePath = resolve-path $xmlFilePath
    $outputFilePath = resolve-path $outputFilePath
-   if( -not (test-path (split-path $originalXmlFilePath)) ) { throw "Can't find the output folder" } 
 
-   ## Get an XSL Transform object (try for the new .Net 3.5 version first)
-   $EAP = $ErrorActionPreference
-   $ErrorActionPreference = "SilentlyContinue"
-   $script:xslt = new-object system.xml.xsl.xslcompiledtransform
-   trap [System.Management.Automation.PSArgumentException] 
-   {  # no 3.5, use the slower 2.0 one
-      $ErrorActionPreference = $EAP
-      $script:xslt = new-object system.xml.xsl.xsltransform
-   }
-   $ErrorActionPreference = $EAP
-   
-   ## load xslt file
-   $xslt.load( $xslFilePath )
-     
-   ## transform 
-   $xslt.Transform( $originalXmlFilePath, $outputFilePath )
+   $xslt = new-object system.xml.xsl.xslcompiledtransform
+   $xslt.load( $xsltFilePath )
+   $xslt.Transform( $xmlFilePath, $outputFilePath )
 }
 
 if(-not $SolutionDir) 
