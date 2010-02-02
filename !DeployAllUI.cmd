30e2182a079a9961cdc1de27a46873c9e2b86502
David Schmitt <david@dasz.at>
2010-02-02 09:56:31 +0000

    always repair on deploy
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@1475 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2

--- !DeployAllUI.cmd
+++ !DeployAllUI.cmd
@@ -1,4 +1,4 @@
 @echo off
-bin\debug\bin\Server\Kistl.Server.Service.exe Kistl.Server.Service\DefaultConfig.xml -deploy Kistl.Server\Database\Database.xml -updateschema -checkschema 
+bin\debug\bin\Server\Kistl.Server.Service.exe Kistl.Server.Service\DefaultConfig.xml -deploy Kistl.Server\Database\Database.xml -updatedeployedschema -repairschema 
 bin\debug\bin\Server\Kistl.Server.Service.exe Kistl.Server.Service\DefaultConfig.xml -generate
 pause
