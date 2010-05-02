91d1d41c586b04c5a00d156ae5cf315b502282f3
Arthur Zaczek <arthur@dasz.at>
2010-05-02 15:33:02 +0000

    fixed dalprovider.frozen tests
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@1710 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2

--- Tests/Kistl.DalProvider.Frozen.Tests/SetUpFixture.cs
+++ Tests/Kistl.DalProvider.Frozen.Tests/SetUpFixture.cs
@@ -2,13 +2,49 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using Autofac;
 
 using NUnit.Framework;
+using Kistl.API;
+
+internal class FrozenAssemblyConfiguration : IAssemblyConfiguration
+{
+    #region IAssemblyConfiguration Members
+
+    public string InterfaceAssemblyName
+    {
+        get { return Kistl.API.Helper.InterfaceAssembly; }
+    }
+
+    public IEnumerable<string> AllImplementationAssemblyNames
+    {
+        get { return new[] { Kistl.API.Helper.FrozenAssembly }; }
+    }
+    #endregion
+}
 
 [SetUpFixture]
 public class SetUpFixture : Kistl.API.AbstractConsumerTests.AbstractSetUpFixture
 {
+    protected override void SetupBuilder(ContainerBuilder builder)
+    {
+        base.SetupBuilder(builder);
+        // Register modules -> Hosttype = none
+        builder.RegisterModule(new Kistl.API.ApiModule());
+
+        builder
+            .RegisterType<FrozenAssemblyConfiguration>()
+            .As<IAssemblyConfiguration>()
+            .SingleInstance();
+
+    }
+
+    protected override void SetUp(Autofac.IContainer container)
+    {
+        base.SetUp(container);
+        FrozenContext.RegisterTypeTransformations(container.Resolve<ITypeTransformations>());
+    }
+
     protected override string GetConfigFile()
     {
         return "Kistl.DalProvider.Frozen.Tests.Config.xml";
