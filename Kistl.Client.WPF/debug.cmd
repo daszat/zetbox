e68df46fe85c5a9d31cb9e87f75562fd2d6aa530
Arthur Zaczek <arthur@dasz.at>
2010-05-21 11:28:28 +0000

    Made every ControlKindClass Class to an ObjectClass
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@1733 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2

---
+++ Kistl.Client.WPF/debug.cmd
@@ -0,0 +1,3 @@
+@echo off
+cd ..
+start bin\debug\bin\Client\Kistl.Client.WPF.exe bin\debug\bin\Client\DefaultConfig.xml
--- Kistl.Client.WPF/debug_arthur.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-start bin\debug\Kistl.Client.WPF.exe ConfigArthur.xml
