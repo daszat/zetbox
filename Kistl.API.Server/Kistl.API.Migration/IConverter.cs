b96eca7c170332ac79840bd108cc480faa4eb165
Arthur Zaczek <arthur@dasz.at>
2010-08-11 14:23:50 +0000

    Mapped Refundierungen Added RegEx to DateTimeConverter
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@1929 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2
    
    Removed customer's generated code and schema

--- Kistl.API.Server/Kistl.API.Migration/IConverter.cs
+++ Kistl.API.Server/Kistl.API.Migration/IConverter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace Kistl.API.Migration
 {
@@ -11,26 +12,72 @@ namespace Kistl.API.Migration
         object Convert(object v);
     }
 
-    public class SqlServerDateTimeConverter : IConverter
+    public class DateTimeConverter : IConverter
     {
-        private static readonly DateTime SqlMinValue = new DateTime(1753, 1, 1);
-        private static readonly DateTime SqlMaxValue = new DateTime(9999, 12, 31);
+        private static readonly Regex RegExYear = new Regex("^(\\d{4})$");
+        private static readonly Regex RegExYearMonth = new Regex("^(\\d{4})/(\\d{2})$");
 
-        public object Convert(object v)
+        public virtual object Convert(object v)
         {
             if (v == null || v == DBNull.Value) return DBNull.Value;
 
             try
             {
-                var result = System.Convert.ToDateTime(v, CultureInfo.GetCultureInfo("de-AT"));
-                if (result < SqlMinValue || result > SqlMaxValue) return DBNull.Value;
-                return result;
+                return System.Convert.ToDateTime(v, CultureInfo.GetCultureInfo("de-AT"));
             }
             catch
             {
-                // ignore, return DBNull
-                return DBNull.Value;
+                // ignore, continue
             }
+
+            try
+            {
+                // Try some regex
+                if (v is string)
+                {
+                    var s = (string)v;
+
+                    var m = RegExYearMonth.Match(s);
+                    if (m != null && m.Groups.Count == 3)
+                    {
+                        return new DateTime(
+                            int.Parse(m.Groups[1].Value),
+                            int.Parse(m.Groups[2].Value),
+                            1);
+                    }
+                    m = RegExYear.Match(s);
+                    if (m != null && m.Groups.Count == 2)
+                    {
+                        return new DateTime(
+                            int.Parse(m.Groups[1].Value),
+                            1,
+                            1);
+                    }
+                }
+            }
+            catch
+            {
+                // ignore, continue
+            }
+
+            return DBNull.Value;
+        }
+    }
+
+    public class SqlServerDateTimeConverter : DateTimeConverter
+    {
+        private static readonly DateTime SqlMinValue = new DateTime(1753, 1, 1);
+        private static readonly DateTime SqlMaxValue = new DateTime(9999, 12, 31);
+
+        public override object Convert(object v)
+        {
+            var result = base.Convert(v);
+            if (result is DateTime)
+            {
+                var d = (DateTime)result;
+                if (d < SqlMinValue || d > SqlMaxValue) return DBNull.Value;
+            }
+            return result;
         }
     }
 }
