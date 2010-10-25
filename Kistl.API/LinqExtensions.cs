eed4e8b2c8036f78b3db73f6614bdc68f4d8ed86
Arthur Zaczek <arthur@dasz.at>
2010-10-25 11:30:05 +0000

    Init50: Calendar
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@2113 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2
    
    Removed customer's generated code and schema

--- Kistl.API/LinqExtensions.cs
+++ Kistl.API/LinqExtensions.cs
@@ -196,6 +196,58 @@ namespace Kistl.API
                 queryable.Expression, orderBy));
         }
 
+        #region // http://stackoverflow.com/questions/110314/linq-to-entities-building-where-clauses-to-test-collections-within-a-many-to-ma#131551
+        public static Expression<Func<T, bool>> True<T>() { return f => true; }
+
+        public static Expression<Func<T, bool>> False<T>() { return f => false; }
+
+        public class ParameterRebinder : ExpressionTreeTranslator
+        {
+            private readonly Dictionary<ParameterExpression, ParameterExpression> map;
+            public ParameterRebinder(Dictionary<ParameterExpression, ParameterExpression> map)
+            {
+                this.map = map ?? new Dictionary<ParameterExpression, ParameterExpression>();
+            }
+            public static Expression ReplaceParameters(Dictionary<ParameterExpression, ParameterExpression> map, Expression exp)
+            {
+                return new ParameterRebinder(map).Visit(exp);
+            }
+            protected override ParameterExpression VisitParameter(ParameterExpression p)
+            {
+                ParameterExpression replacement;
+                if (map.TryGetValue(p, out replacement))
+                {
+                    p = replacement;
+                }
+                return base.VisitParameter(p);
+            }
+        }
+
+        public static Expression<T> Compose<T>(this Expression<T> first, Expression<T> second, Func<Expression, Expression, Expression> merge)
+        {
+            if (first == null) throw new ArgumentNullException("first");
+            if (second == null) throw new ArgumentNullException("second");
+            if (merge == null) throw new ArgumentNullException("merge");
+
+            // build parameter map (from parameters of second to parameters of first)                
+            var map = first.Parameters.Select((f, i) => new { f, s = second.Parameters[i] }).ToDictionary(p => p.s, p => p.f);
+            // replace parameters in the second lambda expression with parameters from the first                
+            var secondBody = ParameterRebinder.ReplaceParameters(map, second.Body);
+            // apply composition of lambda expression bodies to parameters from the first expression                 
+            return Expression.Lambda<T>(merge(first.Body, secondBody), first.Parameters);
+        }
+        public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> first, Expression<Func<T, bool>> second)
+        {
+            if (first == null) throw new ArgumentNullException("first");
+            return first.Compose(second, Expression.And);
+        }
+        public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> first, Expression<Func<T, bool>> second)
+        {
+            if (first == null) throw new ArgumentNullException("first");
+            return first.Compose(second, Expression.Or);
+        }
+        #endregion
+
         /// <summary>
         /// Returns the Value of a Constant or Member Expression
         /// TODO: Wer benutzt das? Ich glaub nur der ClientContext beim Auswerten von Take. Das könnte man vielleicht mit dem Constant Evaluatoer machen
