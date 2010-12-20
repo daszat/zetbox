676c7f1e306b3d6ac62aa8b9eda352fc3918b794
Arthur Zaczek <arthur@dasz.at>
2010-12-20 15:24:06 +0000

    CCNet - PostgreSQL
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@2311 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2
    
    Removed customer's generated code and schema

---
+++ !ResetDatabase.cmd
@@ -0,0 +1,34 @@
+@echo off
+echo ********************************************************************************
+echo Full reset with roundtrip of ZBox Database
+echo Used to initialize a new dev enviromnent
+echo ********************************************************************************
+
+bin\debug\bin\Server\Kistl.Server.Service.exe Kistl.Server.Service\DefaultConfig%zenv%.xml -wipe
+IF ERRORLEVEL 1 GOTO FAIL
+
+bin\debug\bin\Server\Kistl.Server.Service.exe Kistl.Server.Service\DefaultConfig%zenv%.xml -updateschema Kistl.Server\Database\KistlBase.xml KistlBase GUI
+IF ERRORLEVEL 1 GOTO FAIL
+
+bin\debug\bin\Server\Kistl.Server.Service.exe Kistl.Server.Service\DefaultConfig%zenv%.xml -deploy Kistl.Server\Database\Database.xml -checkdeployedschema
+IF ERRORLEVEL 1 GOTO FAIL
+
+bin\debug\bin\Server\Kistl.Server.Service.exe Kistl.Server.Service\DefaultConfig%zenv%.xml -repairschema
+IF ERRORLEVEL 1 GOTO FAIL
+
+bin\debug\bin\Server\Kistl.Server.Service.exe Kistl.Server.Service\DefaultConfig%zenv%.xml -syncidentities
+IF ERRORLEVEL 1 GOTO FAIL
+
+echo ********************************************************************************
+echo ************************************ Success ***********************************
+echo ********************************************************************************
+GOTO EOF
+
+:FAIL
+echo ********************************************************************************
+echo ************************************* FAIL *************************************
+echo ********************************************************************************
+echo Aborting reset.
+
+:EOF
+
