a68969877efd23a1962c2581adc64256fddb1737
David Schmitt <david@dasz.at>
2010-12-27 10:43:20 +0000

    activate debugging
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@2408 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2
    
    Removed customer's generated code and schema

--- !ResetDatabase.sh
+++ !ResetDatabase.sh
@@ -4,10 +4,10 @@ echo '**************************************************************************
 echo 'Reset the database to what is defined in the database.xml'
 echo '********************************************************************************'
 
-mono bin/Debug/bin/Server/Kistl.Server.Service.exe Kistl.Server.Service/DefaultConfig${zenv}.xml -wipe
-mono bin/Debug/bin/Server/Kistl.Server.Service.exe Kistl.Server.Service/DefaultConfig${zenv}.xml -updateschema Kistl.Server/Database/Database.xml
-mono bin/Debug/bin/Server/Kistl.Server.Service.exe Kistl.Server.Service/DefaultConfig${zenv}.xml -deploy Kistl.Server/Database/Database.xml -checkdeployedschema
-mono bin/Debug/bin/Server/Kistl.Server.Service.exe Kistl.Server.Service/DefaultConfig${zenv}.xml -repairschema -syncidentities
+mono --debug bin/Debug/bin/Server/Kistl.Server.Service.exe Kistl.Server.Service/DefaultConfig${zenv}.xml -wipe
+mono --debug bin/Debug/bin/Server/Kistl.Server.Service.exe Kistl.Server.Service/DefaultConfig${zenv}.xml -updateschema Kistl.Server/Database/Database.xml
+mono --debug bin/Debug/bin/Server/Kistl.Server.Service.exe Kistl.Server.Service/DefaultConfig${zenv}.xml -deploy Kistl.Server/Database/Database.xml -checkdeployedschema
+mono --debug bin/Debug/bin/Server/Kistl.Server.Service.exe Kistl.Server.Service/DefaultConfig${zenv}.xml -repairschema -syncidentities
 
 echo '********************************************************************************'
 echo '************************************ Success ***********************************'
