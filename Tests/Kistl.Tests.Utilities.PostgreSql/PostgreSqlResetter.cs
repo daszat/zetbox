a02681f41d6b0f4d6e083b89efbe56882cb5e8a6
David Schmitt <david@dasz.at>
2011-01-21 12:35:15 +0000

    Revert local changes
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@2538 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2
    
    Removed customer's generated code and schema

--- Tests/Kistl.Tests.Utilities.PostgreSql/PostgreSqlResetter.cs
+++ Tests/Kistl.Tests.Utilities.PostgreSql/PostgreSqlResetter.cs
@@ -7,13 +7,13 @@ namespace Kistl.Tests.Utilities.PostgresSql
     using System.Linq;
     using System.Text;
     using System.Text.RegularExpressions;
-    using Autofac;
     using Kistl.API.AbstractConsumerTests;
     using Kistl.API.Configuration;
-    using Kistl.API.Server;
     using Kistl.API.Utils;
     using Npgsql;
     using NUnit.Framework;
+    using Kistl.API.Server;
+    using Autofac;
 
     public sealed class PostgreSqlResetter
         : IDatabaseResetter
@@ -40,27 +40,27 @@ namespace Kistl.Tests.Utilities.PostgresSql
                 try
                 {
                     Log.Info("Restoring Database");
-                    //schemaProvider.Open(config.Server.ConnectionString);
-                    //schemaProvider.DropAllObjects();
+                    schemaProvider.Open(config.Server.ConnectionString);
+                    schemaProvider.DropAllObjects();
 
-                    //var cb = new NpgsqlConnectionStringBuilder(config.Server.ConnectionString);
-                    //var srcDB = cb.Database.Substring(0, cb.Database.Length - "_test".Length);
-                    //var destDB = cb.Database;
-                    //var userCmdString = "-U postgres -w";
-                    //var args = string.Format("/C pg_dump {0} {1} | psql {0} {2}", userCmdString, srcDB, destDB);
-                    //System.Diagnostics.ProcessStartInfo pi = new System.Diagnostics.ProcessStartInfo("cmd.exe", args);
-                    //pi.UseShellExecute = false;
-                    //var pgSQLBinPath = Environment.GetEnvironmentVariable("PGSQLBinPath");
-                    //if (string.IsNullOrEmpty(pgSQLBinPath))
-                    //{
-                    //    throw new InvalidOperationException("Environment Variable PGSQLBinPath is not set, unable to reset test database");
-                    //}
-                    //pi.WorkingDirectory = pgSQLBinPath.Trim('\"');
-                    //var p = System.Diagnostics.Process.Start(pi);
-                    //if (!p.WaitForExit(RESET_TIMEOUT * 1000))
-                    //{
-                    //    throw new InvalidOperationException(string.Format("pg_dump did not completed within {0} seconds", RESET_TIMEOUT));
-                    //}
+                    var cb = new NpgsqlConnectionStringBuilder(config.Server.ConnectionString);
+                    var srcDB = cb.Database.Substring(0, cb.Database.Length - "_test".Length);
+                    var destDB = cb.Database;
+                    var userCmdString = "-U postgres -w";
+                    var args = string.Format("/C pg_dump {0} {1} | psql {0} {2}", userCmdString, srcDB, destDB);
+                    System.Diagnostics.ProcessStartInfo pi = new System.Diagnostics.ProcessStartInfo("cmd.exe", args);
+                    pi.UseShellExecute = false;
+                    var pgSQLBinPath = Environment.GetEnvironmentVariable("PGSQLBinPath");
+                    if (string.IsNullOrEmpty(pgSQLBinPath))
+                    {
+                        throw new InvalidOperationException("Environment Variable PGSQLBinPath is not set, unable to reset test database");
+                    }
+                    pi.WorkingDirectory = pgSQLBinPath.Trim('\"');
+                    var p = System.Diagnostics.Process.Start(pi);
+                    if (!p.WaitForExit(RESET_TIMEOUT * 1000))
+                    {
+                        throw new InvalidOperationException(string.Format("pg_dump did not completed within {0} seconds", RESET_TIMEOUT));
+                    }
 
                     // After recreating the database, all connection pools should be cleard
                     NpgsqlConnection.ClearAllPools();
