f102aff42729225d66d5119082abbd7626585656
Arthur Zaczek <arthur@dasz.at>
2011-01-24 09:01:07 +0000

    TempAppHelper -> Kistl.API.Common
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@2549 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2
    
    Removed customer's generated code and schema

--- Kistl.API.Common/Properties/AssemblyInfo.cs
+++ Kistl.API.Common/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TempAppHelpers")]
+[assembly: AssemblyTitle("Kistl.API.Common")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("dasz.at OG")]
-[assembly: AssemblyProduct("TempAppHelpers")]
+[assembly: AssemblyProduct("Kistl.API.Common")]
 [assembly: AssemblyCopyright("Copyright © dasz.at OG 2007-2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
