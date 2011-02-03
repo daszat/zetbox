d15d314dd7f5a100a8aea39351de1dbb87790da7
David Schmitt <david@dasz.at>
2011-02-03 07:52:29 +0000

    [Ef] expect EntityCollection in collection type test
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@2679 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2
    
    Removed customer's generated code and schema

--- Tests/Kistl.DalProvider.EF.Tests/Tests/N_to_M_relations/when_initializing.cs
+++ Tests/Kistl.DalProvider.EF.Tests/Tests/N_to_M_relations/when_initializing.cs
@@ -3,6 +3,7 @@ namespace Kistl.DalProvider.Ef.Tests.N_to_M_relations
 {
     using System;
     using System.Collections.Generic;
+    using System.Data.Objects.DataClasses;
     using System.Linq;
     using System.Text;
     using Kistl.API;
@@ -16,8 +17,8 @@ namespace Kistl.DalProvider.Ef.Tests.N_to_M_relations
         [Test]
         public void should_be_of_proper_type()
         {
-            Assert.That(aSide1.BSide, Is.TypeOf<BSideCollectionWrapper<N_to_M_relations_A, N_to_M_relations_B, N_to_M_relations_A_connectsTo_N_to_M_relations_B_RelationEntryEfImpl, ICollection<N_to_M_relations_A_connectsTo_N_to_M_relations_B_RelationEntryEfImpl>>>());
-            Assert.That(bSide1.ASide, Is.TypeOf<ASideCollectionWrapper<N_to_M_relations_A, N_to_M_relations_B, N_to_M_relations_A_connectsTo_N_to_M_relations_B_RelationEntryEfImpl, ICollection<N_to_M_relations_A_connectsTo_N_to_M_relations_B_RelationEntryEfImpl>>>());
+            Assert.That(aSide1.BSide, Is.TypeOf<BSideCollectionWrapper<N_to_M_relations_A, N_to_M_relations_B, N_to_M_relations_A_connectsTo_N_to_M_relations_B_RelationEntryEfImpl, EntityCollection<N_to_M_relations_A_connectsTo_N_to_M_relations_B_RelationEntryEfImpl>>>());
+            Assert.That(bSide1.ASide, Is.TypeOf<ASideCollectionWrapper<N_to_M_relations_A, N_to_M_relations_B, N_to_M_relations_A_connectsTo_N_to_M_relations_B_RelationEntryEfImpl, EntityCollection<N_to_M_relations_A_connectsTo_N_to_M_relations_B_RelationEntryEfImpl>>>());
         }
 
         public class and_reloading : when_initializing
