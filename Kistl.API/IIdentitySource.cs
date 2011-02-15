439219f72c1ed2699a141f707afdb8446d3ada2d
David Schmitt <david@dasz.at>
2011-02-15 15:15:52 +0000

    * Kistl.API/IIdentitySource.cs: add missing file
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@2781 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2
    
    Removed customer's generated code and schema

---
+++ Kistl.API/IIdentitySource.cs
@@ -0,0 +1,18 @@
+
+namespace Kistl.API
+{
+	using System;
+	using System.Collections.Generic;
+	
+    public class IdentitySourceItem
+    {
+        public string UserName { get; set; }
+        public string DisplayName { get; set; }
+    }
+
+	public interface IIdentitySource
+    {
+        IEnumerable<IdentitySourceItem> GetAllIdentities();
+    }
+}
+
