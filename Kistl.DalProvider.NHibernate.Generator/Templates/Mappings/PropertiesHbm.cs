1833a7eebe7893c70368dda07086cd1ae5b25ea7
David Schmitt <david@dasz.at>
2011-02-16 15:51:12 +0000

    [NHibernate] fix mapping of value type list properties
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@2804 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2
    
    Removed customer's generated code and schema

--- Kistl.DalProvider.NHibernate.Generator/Templates/Mappings/PropertiesHbm.cs
+++ Kistl.DalProvider.NHibernate.Generator/Templates/Mappings/PropertiesHbm.cs
@@ -174,7 +174,17 @@ namespace Kistl.DalProvider.NHibernate.Generator.Templates.Mappings
             string nameAttr = String.Format("name=\"{0}\"", prop.Name);
             string tableName = prop.GetCollectionEntryTable();
             string tableAttr = String.Format("table=\"`{0}`\"", tableName);
-            string typeAttr = String.Empty; // NHibernate via reflection String.Format("type=\"{0}\"", prop.GetPropertyTypeString());
+            string typeAttr;
+            if (prop.IsList)
+            {
+                // set the proper type for relation entries
+                typeAttr = String.Format("type=\"{0}\"", prop.GetPropertyTypeString());
+            }
+            else
+            {
+                // let NHibernate work it out via reflection
+                typeAttr = String.Empty; 
+            }
             string mappingType = prop.HasPersistentOrder ? "list" : "idbag";
 
             if (prop.IsList)
