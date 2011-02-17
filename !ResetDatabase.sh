3bd5ae1c7bc58348ca767d82330604dceb63f910
David Schmitt <david@dasz.at>
2011-02-17 14:46:26 +0000

    [Linux] fix slash/backslash confusion backlash
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@2833 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2
    
    Removed customer's generated code and schema

--- !ResetDatabase.sh
+++ !ResetDatabase.sh
@@ -4,10 +4,10 @@ echo '**************************************************************************
 echo 'Reset the database to what is defined in the database.xml'
 echo '********************************************************************************'
 
-mono --debug bin/Debug/bin/Server/Kistl.Server.Service.exe Configs\$zenv\Kistl.Server.Service.xml -wipe
-mono --debug bin/Debug/bin/Server/Kistl.Server.Service.exe Configs\$zenv\Kistl.Server.Service.xml -updateschema Kistl.Server/Database/Database.xml
-mono --debug bin/Debug/bin/Server/Kistl.Server.Service.exe Configs\$zenv\Kistl.Server.Service.xml -deploy Kistl.Server/Database/Database.xml -checkdeployedschema
-mono --debug bin/Debug/bin/Server/Kistl.Server.Service.exe Configs\$zenv\Kistl.Server.Service.xml -repairschema -syncidentities
+mono --debug bin/Debug/bin/Server/Kistl.Server.Service.exe Configs/$zenv/Kistl.Server.Service.xml -wipe
+mono --debug bin/Debug/bin/Server/Kistl.Server.Service.exe Configs/$zenv/Kistl.Server.Service.xml -updateschema Kistl.Server/Database/Database.xml
+mono --debug bin/Debug/bin/Server/Kistl.Server.Service.exe Configs/$zenv/Kistl.Server.Service.xml -deploy Kistl.Server/Database/Database.xml -checkdeployedschema
+mono --debug bin/Debug/bin/Server/Kistl.Server.Service.exe Configs/$zenv/Kistl.Server.Service.xml -repairschema -syncidentities
 
 echo '********************************************************************************'
 echo '************************************ Success ***********************************'
