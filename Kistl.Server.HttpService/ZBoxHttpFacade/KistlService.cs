8f695fa92e127cbd6b4447b9e7606e2f284826dc
David Schmitt <david@dasz.at>
2011-02-24 11:48:47 +0000

    [HttpService] Testimplementation for KistlService.facade
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@2889 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2
    
    Removed customer's generated code and schema

--- Kistl.Server.HttpService/ZBoxHttpFacade/KistlService.cs
+++ Kistl.Server.HttpService/ZBoxHttpFacade/KistlService.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.IO;
 
 namespace Kistl.Server.HttpService
 {
@@ -14,7 +15,25 @@ namespace Kistl.Server.HttpService
 
         public void ProcessRequest(HttpContext context)
         {
-            // Add the shit
+            switch (context.Request.Url.Segments.Last())
+            {
+                case "SetObjects":
+                case "GetList":
+                case "GetListOf":
+                case "FetchRelation":
+                case "GetBlobStream":
+                case "SetBlobStream":
+                case "InvokeServerMethod":
+                    context.Response.StatusCode = 200;
+                    context.Response.ContentType = "text/plain";
+                    using (var outStream = new StreamWriter(context.Response.OutputStream))
+                    {
+                        outStream.WriteLine("haha!");
+                    }
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
         }
     }
 }
