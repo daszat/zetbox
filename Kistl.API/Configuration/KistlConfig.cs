c681de041bd74c7d54125b73943ac151a829df44
David Schmitt <david@dasz.at>
2011-03-02 15:19:48 +0000

    [API] do not insert "Configs" when specifying rooted zenv
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@2961 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2
    
    Removed customer's generated code and schema

--- Kistl.API/Configuration/KistlConfig.cs
+++ Kistl.API/Configuration/KistlConfig.cs
@@ -310,15 +310,15 @@ namespace Kistl.API.Configuration
                 zenv = "Local";
             }
             var file = Path.IsPathRooted(zenv)
-                ? Path.Combine(Path.Combine(zenv, baseDir), basename)
-                : Path.GetFullPath(Path.Combine(Path.Combine(baseDir, zenv), basename));
+                ? Path.Combine(zenv, basename)
+                : Path.GetFullPath(Helper.PathCombine(baseDir, zenv, basename));
             Logging.Log.InfoFormat("Got zenv=[{0}], trying file=[{1}]", zenv, file);
             while (!Path.IsPathRooted(zenv) && !File.Exists(file) && !String.IsNullOrEmpty(zenv))
             {
                 Logging.Log.InfoFormat("rooted={0}, exists={1}, zenv_empty={2}", Path.IsPathRooted(zenv), File.Exists(file), String.IsNullOrEmpty(zenv));
                 // this will reduce zenv directory component-wise until nothing is left
                 zenv = Path.GetDirectoryName(zenv);
-                file = Path.GetFullPath(Path.Combine(Path.Combine(baseDir, zenv), basename));
+                file = Path.GetFullPath(Helper.PathCombine(baseDir, zenv, basename));
                 Logging.Log.InfoFormat("Got zenv=[{0}], trying file=[{1}]", zenv, file);
             }
             if (!File.Exists(file))
