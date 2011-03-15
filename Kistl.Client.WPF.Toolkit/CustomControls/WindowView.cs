eda2e4ccde1aabe03b89a345b9b5faebbca07317
David Schmitt <david@dasz.at>
2011-03-15 12:47:47 +0000

    Change closing control flow so that the user is asked exactly once when there is unsaved data.
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@3079 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2
    
    Removed customer's generated code and schema

--- Kistl.Client.WPF.Toolkit/CustomControls/WindowView.cs
+++ Kistl.Client.WPF.Toolkit/CustomControls/WindowView.cs
@@ -15,14 +15,28 @@ namespace Kistl.Client.WPF.CustomControls
             this.Closing += new System.ComponentModel.CancelEventHandler(WindowView_Closing);
         }
 
+        private bool _closing;
         void WindowView_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            if (WindowViewModel != null)
+            if (WindowViewModel != null && WindowViewModel.Show)
             {
-                e.Cancel = !WindowViewModel.CanClose();
+                _closing = true;
+                // we're coming from WPF, e.g. clicking the red X or calling Close()
+                // now we need to tell the view model that it should shut down
+                WindowViewModel.Show = false;
+
+                // the ViewModel might want to ignore our signal
+                // then we should not close
+                if (WindowViewModel.Show)
+                {
+                    e.Cancel = true;
+                    _closing = false;
+                }
             }
             else
             {
+                // either we do not have a view model, or the view model is alread in shut down
+                // both mean that we just have to close
                 e.Cancel = false;
             }
         }
@@ -37,7 +51,7 @@ namespace Kistl.Client.WPF.CustomControls
 
         void WindowView_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
-            if (e.PropertyName == "Show" && !WindowViewModel.Show) this.Close();
+            if (e.PropertyName == "Show" && !_closing && !WindowViewModel.Show) this.Close();
         }
 
         private WindowViewModel WindowViewModel
