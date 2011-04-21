7098dafc3dcc5b80a7df88aab10189f10f85c2c3
Arthur Zaczek <arthur@dasz.at>
2011-04-21 13:49:56 +0000

    2502
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@3280 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2
    
    Removed customer's generated code and schema

--- Kistl.DalProvider.EF.Generator/Templates/CollectionEntries/ValueCollectionEntry.cs
+++ Kistl.DalProvider.EF.Generator/Templates/CollectionEntries/ValueCollectionEntry.cs
@@ -19,10 +19,49 @@ namespace Kistl.DalProvider.Ef.Generator.Templates.CollectionEntries
         {
         }
 
+        protected override void ApplyConstructorTemplate()
+        {
+            // replace base constructors
+            //base.ApplyConstructorTemplate();
+
+            this.WriteObjects("[Obsolete]");
+            this.WriteLine();
+            this.WriteObjects("public ", GetCeClassName(), "()");
+            this.WriteLine();
+            this.WriteObjects(": base(null)");
+            this.WriteLine();
+            this.WriteObjects("{");
+            this.WriteLine();
+
+            if (this.prop is CompoundObjectProperty)
+            {
+                Templates.Properties.CompoundObjectPropertyInitialisation.Call(Host, ctx, prop.GetPropertyTypeString() + ImplementationSuffix, "Value", "_Value", false);
+            }
+
+            this.WriteObjects("}");
+            this.WriteLine();
+
+            this.WriteObjects("public ", GetCeClassName() ,"(Func<IFrozenContext> lazyCtx)");
+            this.WriteLine();
+            this.WriteObjects("    : base(lazyCtx)");
+            this.WriteLine();
+            this.WriteObjects("{");
+            this.WriteLine();
+            
+            if (this.prop is CompoundObjectProperty)
+            {
+                Templates.Properties.CompoundObjectPropertyInitialisation.Call(Host, ctx, prop.GetPropertyTypeString() + ImplementationSuffix, "Value", "_Value", false);
+            }
+
+            this.WriteObjects("}");
+            this.WriteLine();
+
+        }
+
         protected override void ApplyClassAttributeTemplate()
         {
             base.ApplyClassAttributeTemplate();
-            this.WriteObjects(@"    [EdmEntityType(NamespaceName=""Model"", Name=""", 
+            this.WriteObjects(@"    [EdmEntityType(NamespaceName=""Model"", Name=""",
                 prop.GetCollectionEntryClassName(), @""")]");
             this.WriteLine();
         }
