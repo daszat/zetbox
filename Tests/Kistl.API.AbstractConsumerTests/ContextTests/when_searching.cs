61b85c339ba17fa87805df10113494a73416a320
David Schmitt <david@dasz.at>
2011-05-30 12:08:07 +0000

    Case 2686: "implement client-side Skip/Take" unit test
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@3510 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2
    
    Removed customer's generated code and schema

--- Tests/Kistl.API.AbstractConsumerTests/ContextTests/when_searching.cs
+++ Tests/Kistl.API.AbstractConsumerTests/ContextTests/when_searching.cs
@@ -42,5 +42,30 @@ namespace Kistl.API.AbstractConsumerTests.ContextTests
             var foundGuids = foundObjs.Select(cls => cls.ExportGuid);
             Assert.That(foundGuids, Is.EquivalentTo(guids));
         }
+
+        [Test]
+        [Ignore("Case 2686: implement client-side Skip/Take")]
+        public void and_skipping()
+        {
+            var ctx = GetContext();
+            var baseQuery = ctx.GetQuery<ObjectClass>().OrderBy(o => o.ID);
+            var objs = baseQuery.Take(10).ToList();
+            Assert.That(objs.Count, Is.EqualTo(10), "Did not receive correct amount of objects after Take(10)");
+            var firstHalf = baseQuery.Take(5).ToList();
+            Assert.That(firstHalf.Count, Is.EqualTo(5), "Did not receive correct amount of objects after Take(5)");
+            var secondHalf = baseQuery.Skip(5).Take(5).ToList();
+            Assert.That(secondHalf.Count, Is.EqualTo(5), "Did not receive correct amount of objects after Skip(5).Take(5)");
+
+            Assert.That(firstHalf[0], Is.SameAs(objs[0]), "0");
+            Assert.That(firstHalf[1], Is.SameAs(objs[1]), "1");
+            Assert.That(firstHalf[2], Is.SameAs(objs[2]), "2");
+            Assert.That(firstHalf[3], Is.SameAs(objs[3]), "3");
+            Assert.That(firstHalf[4], Is.SameAs(objs[4]), "4");
+            Assert.That(secondHalf[0], Is.SameAs(objs[5]), "5");
+            Assert.That(secondHalf[1], Is.SameAs(objs[6]), "6");
+            Assert.That(secondHalf[2], Is.SameAs(objs[7]), "7");
+            Assert.That(secondHalf[3], Is.SameAs(objs[8]), "8");
+            Assert.That(secondHalf[4], Is.SameAs(objs[9]), "9");
+        }
     }
 }
