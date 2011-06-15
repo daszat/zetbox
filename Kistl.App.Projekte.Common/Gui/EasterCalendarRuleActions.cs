bed3c18aef9d72ed2917860e993f45ab842d8442
Arthur Zaczek <arthur@dasz.at>
2011-06-15 08:50:05 +0000

    Calculate easter
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@3593 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2
    
    Removed customer's generated code and schema

--- Kistl.App.Projekte.Common/Gui/EasterCalendarRuleActions.cs
+++ Kistl.App.Projekte.Common/Gui/EasterCalendarRuleActions.cs
@@ -19,7 +19,35 @@ namespace Kistl.App.GUI
         [Invocation]
         public static void AppliesTo(EasterCalendarRule obj, MethodReturnEventArgs<System.Boolean> e, System.DateTime date)
         {
-            e.Result = false;
+            var ostern = GetOstersonntag(date.Year);
+            e.Result = ostern.AddDays(obj.Offset ?? 0) == date.Date;
+        }
+
+        private static DateTime GetOstersonntag(int jahr)
+        {
+            int c;
+            int i;
+            int j;
+            int k;
+            int l;
+            int n;
+            int osterTag;
+            int osterMonat;
+
+            c = jahr / 100;
+            n = jahr % 19;
+            k = (c - 17) / 25;
+            i = c - c / 4 - ((c - k) / 3) + 19 * n + 15;
+            i = i % 30;
+            i = i - (i / 28) * ((1 - (i / 28)) * ((29 / (i + 1))) * ((21 - n) / 11));
+            j = jahr + (jahr / 4) + i + 2 - c + (c / 4);
+            j = j % 7;
+            l = i - j;
+
+            osterMonat = 3 + ((l + 40) / 44);
+            osterTag = l + 28 - 31 * (osterMonat / 4);
+
+            return new DateTime(jahr, osterMonat, osterTag);
         }
     }
 }
