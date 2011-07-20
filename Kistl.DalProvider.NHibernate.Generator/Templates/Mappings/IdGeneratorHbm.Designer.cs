c6caa73cab2971e25204bc6e3b9c528327f727d1
Arthur Zaczek <arthur@dasz.at>
2011-07-20 07:26:20 +0000

    case 3011: Fix nHibernate save order problem local, a reset database was OK
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@3750 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2
    
    Removed customer's generated code and schema

--- Kistl.DalProvider.NHibernate.Generator/Templates/Mappings/IdGeneratorHbm.Designer.cs
+++ Kistl.DalProvider.NHibernate.Generator/Templates/Mappings/IdGeneratorHbm.Designer.cs
@@ -32,7 +32,7 @@ switch(idType) {
 #line 9 "P:\Kistl\Kistl.DalProvider.NHibernate.Generator\Templates\Mappings\IdGeneratorHbm.cst"
 case "id":                                                              
 #line 10 "P:\Kistl\Kistl.DalProvider.NHibernate.Generator\Templates\Mappings\IdGeneratorHbm.cst"
-this.WriteObjects("        <id name=\"ID\" column=\"`ID`\" type=\"Int32\">\r\n");
+this.WriteObjects("        <id name=\"ID\" column=\"`ID`\" type=\"Int32\" unsaved-value=\"0\">\r\n");
 this.WriteObjects("            <generator class=\"native\">\r\n");
 this.WriteObjects("                <param name=\"sequence\">`",  tableName , "_ID_seq`</param>\r\n");
 this.WriteObjects("            </generator>\r\n");
@@ -42,7 +42,7 @@ break;
 #line 16 "P:\Kistl\Kistl.DalProvider.NHibernate.Generator\Templates\Mappings\IdGeneratorHbm.cst"
 case "collection-id":                                                   
 #line 17 "P:\Kistl\Kistl.DalProvider.NHibernate.Generator\Templates\Mappings\IdGeneratorHbm.cst"
-this.WriteObjects("        <collection-id column=\"`ID`\" type=\"Int32\">\r\n");
+this.WriteObjects("        <collection-id column=\"`ID`\" type=\"Int32\" unsaved-value=\"0\">\r\n");
 this.WriteObjects("            <generator class=\"native\">\r\n");
 this.WriteObjects("                <param name=\"sequence\">`",  tableName , "_ID_seq`</param>\r\n");
 this.WriteObjects("            </generator>\r\n");
