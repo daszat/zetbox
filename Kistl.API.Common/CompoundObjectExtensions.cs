412e5dd6ee69ae16252b666543249926e150065f
Arthur Zaczek <arthur@dasz.at>
2011-07-21 10:18:20 +0000

    another perf improvement, cache CompoundObject Lookups
    git-svn-id: svn+ssh://dasz.at/srv/dasz/svn/Kistl@3777 76cbe2a5-d633-47ad-8992-a0d0e66ffbf2
    
    Removed customer's generated code and schema

--- Kistl.API.Common/CompoundObjectExtensions.cs
+++ Kistl.API.Common/CompoundObjectExtensions.cs
@@ -17,18 +17,16 @@ namespace Kistl.App.Extensions
             if (obj == null) { throw new ArgumentNullException("obj"); }
             if (ctx == null) { throw new ArgumentNullException("ctx"); }
 
-            //return GetCompoundObjectDefinition(((BaseCompoundObject)obj).ParentObject.ReadOnlyContext.GetInterfaceType(obj), ctx);
             return GetCompoundObjectDefinition(ctx.GetInterfaceType(obj), ctx);
         }
 
         public static CompoundObject GetCompoundObjectDefinition(this InterfaceType ifType, IReadOnlyKistlContext ctx)
         {
-            // if (ifType == null) { throw new ArgumentNullException("ifType"); }
             if (ctx == null) { throw new ArgumentNullException("ctx"); }
 
             Type type = ifType.Type;
             CompoundObject result;
-            result = ctx.GetQuery<CompoundObject>().First(o => o.Module.Namespace == type.Namespace && o.Name == type.Name);
+            result = ctx.TransientState("__CompoundObjectExtensions__GetCompoundObjectDefinition__", type, () => ctx.GetQuery<CompoundObject>().First(o => o.Module.Namespace == type.Namespace && o.Name == type.Name));
 
             return result;
         }
