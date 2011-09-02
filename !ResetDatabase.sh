968e0aaff42fdff03f9f2a350fd56585e2a192c7
David Schmitt <david@dasz.at>
2011-09-02 14:15:08 +0200

    Update buildscript for Linux CCNet

--- !ResetDatabase.sh
+++ !ResetDatabase.sh
@@ -4,10 +4,13 @@ echo '**************************************************************************
 echo 'Reset the database to what is defined in the database.xml'
 echo '********************************************************************************'
 
-mono --debug bin/Debug/Kistl.Server.Service.exe Configs/$zenv/Kistl.Server.Service.xml -wipe
-mono --debug bin/Debug/Kistl.Server.Service.exe Configs/$zenv/Kistl.Server.Service.xml -updateschema Kistl.Server/Database/Database.xml
-mono --debug bin/Debug/Kistl.Server.Service.exe Configs/$zenv/Kistl.Server.Service.xml -deploy Kistl.Server/Database/Database.xml -checkdeployedschema
-mono --debug bin/Debug/Kistl.Server.Service.exe Configs/$zenv/Kistl.Server.Service.xml -repairschema -syncidentities #-generate
+mono --debug bin/Debug/Kistl.Server.Service.exe Configs/$zenv/Kistl.Server.Service.xml \
+	-updateschema Modules\KistlBasic.xml;Modules\KistlUtils.xml;Modules\TestModules.xml \
+	-deploy Modules\KistlBasic.xml \
+	-deploy Modules\KistlUtils.xml \
+	-deploy Modules\TestModules.xml \
+	-updatedeployedschema \
+	-repairschema
 
 echo '********************************************************************************'
 echo '************************************ Success ***********************************'
