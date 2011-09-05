28e15363434affda61856790dcbb56744b368982
David Schmitt <david@dasz.at>
2011-09-05 10:09:10 +0200

    Fix slashity in linux build script

--- !ResetDatabase.sh
+++ !ResetDatabase.sh
@@ -5,10 +5,10 @@ echo 'Reset the database to what is defined in the database.xml'
 echo '********************************************************************************'
 
 mono --debug bin/Debug/Kistl.Server.Service.exe Configs/$zenv/Kistl.Server.Service.xml \
-	-updateschema Modules\KistlBasic.xml;Modules\KistlUtils.xml;Modules\TestModules.xml \
-	-deploy Modules\KistlBasic.xml \
-	-deploy Modules\KistlUtils.xml \
-	-deploy Modules\TestModules.xml \
+	-updateschema Modules/KistlBasic.xml;Modules/KistlUtils.xml;Modules/TestModules.xml \
+	-deploy Modules/KistlBasic.xml \
+	-deploy Modules/KistlUtils.xml \
+	-deploy Modules/TestModules.xml \
 	-updatedeployedschema \
 	-repairschema
 
