bc2bec8af719b50901687aa056ff23b15ec9b164
David Schmitt <david@dasz.at>
2011-10-03 13:58:49 +0200

    Deploy Tests DLLs.
    since robocopy cannot exclude directories on the top-level only,
    we have to delete this after copying

--- Kistl.Server.Service/ZbLocalPull.cmd
+++ Kistl.Server.Service/ZbLocalPull.cmd
@@ -15,9 +15,16 @@ if .%2. == .. GOTO NO_DESTINATION
 set destination=%2
 :NO_DESTINATION
 
-robocopy %source%\bin\Debug %destination% /MIR /PURGE /XD Tests "*Core.Generated" "*NH.Generated" "*EF.Generated"
+robocopy %source%\bin\Debug %destination% /MIR /XD Core.Generated NH.Generated EF.Generated
 rem errorlevel 8 or higher indicates errors
 IF ERRORLEVEL 8 GOTO FAIL
+
+rem since robocopy cannot exclude directories on the top-level only,
+rem we have to delete this after copying
+del /q /s /f %destination%\Tests
+IF ERRORLEVEL 1 GOTO FAIL
+rd /q /s %destination%\Tests
+IF ERRORLEVEL 1 GOTO FAIL
 
 robocopy %source%\Modules %destination%\Modules /MIR
 rem errorlevel 8 or higher indicates errors
