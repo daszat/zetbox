9d6bac37b0fb79f90352415e51b7a98f89a30b2d
David Schmitt <david@dasz.at>
2011-11-02 10:45:19 +0100

    Add HasGenericDefinition
    This is code by "it depends" http://stackoverflow.com/users/53252/it-depends
    copied from http://stackoverflow.com/questions/457676/c-sharp-reflection-check-if-a-class-is-derived-from-a-generic-class/897388#897388
    checking whether a type has a specific generic type definition in its ancestry.

--- Kistl.API/Helper.cs
+++ Kistl.API/Helper.cs
@@ -202,6 +202,46 @@ namespace Kistl.API
             if (type == null) return false;
             return type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>) && type.GetGenericArguments().Single().IsEnum;
         }
+
+        #region HasGenericDefinition
+        // From: http://stackoverflow.com/questions/457676/c-sharp-reflection-check-if-a-class-is-derived-from-a-generic-class/897388#897388
+
+        /// <summary>
+        /// Checks whether this type has the specified definition in its ancestry.
+        /// </summary>
+        public static bool HasGenericDefinition(this Type type, Type definition)
+        {
+            return GetTypeWithGenericDefinition(type, definition) != null;
+        }
+
+        /// <summary>
+        /// Returns the actual type implementing the specified definition from the ancestry of the type, if available. Else, null.
+        /// </summary>
+        public static Type GetTypeWithGenericDefinition(this Type type, Type definition)
+        {
+            if (type == null)
+                throw new ArgumentNullException("type");
+            if (definition == null)
+                throw new ArgumentNullException("definition");
+            if (!definition.IsGenericTypeDefinition)
+                throw new ArgumentException(
+                    "The definition needs to be a GenericTypeDefinition", "definition");
+
+            if (definition.IsInterface)
+                foreach (var interfaceType in type.GetInterfaces())
+                    if (interfaceType.IsGenericType
+                        && interfaceType.GetGenericTypeDefinition() == definition)
+                        return interfaceType;
+
+            for (Type t = type; t != null; t = t.BaseType)
+                if (t.IsGenericType
+                    && t.GetGenericTypeDefinition() == definition)
+                    return t;
+
+            return null;
+        }
+
+        #endregion
     }
 
     /// <summary>
