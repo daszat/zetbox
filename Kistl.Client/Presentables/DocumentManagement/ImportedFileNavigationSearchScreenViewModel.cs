60defde8f51f423b3a41ca26ddc940df26c5d131
Arthur Zaczek <arthur@dasz.at>
2011-11-04 12:03:15 +0100

    Improving ImportedFileNavigationSearchScreenViewModel

--- Kistl.Client/Presentables/DocumentManagement/ImportedFileNavigationSearchScreenViewModel.cs
+++ Kistl.Client/Presentables/DocumentManagement/ImportedFileNavigationSearchScreenViewModel.cs
@@ -16,13 +16,52 @@ namespace Kistl.Client.Presentables.DocumentManagement
     {
         public new delegate ImportedFileNavigationSearchScreenViewModel Factory(IKistlContext dataCtx, ViewModel parent, NavigationScreen screen);
 
+        private readonly Func<IKistlContext> _ctxFactory;
+
         public ImportedFileNavigationSearchScreenViewModel(IViewModelDependencies appCtx, Func<IKistlContext> ctxFactory,
             IKistlContext dataCtx, ViewModel parent, NavigationScreen screen)
             : base(appCtx, dataCtx, ctxFactory, parent, screen)
         {
+            _ctxFactory = ctxFactory;
+
             base.Type = typeof(ImportedFile).GetObjectClass(FrozenContext);
             base.ListViewModel.ViewMethod = InstanceListViewMethod.Details;
             base.ListViewModel.AllowAddNew = false;
+            base.ListViewModel.Commands.Add(OpenAllCommand);
+        }
+
+        private ICommandViewModel _OpenAllCommand = null;
+        public ICommandViewModel OpenAllCommand
+        {
+            get
+            {
+                if (_OpenAllCommand == null)
+                {
+                    _OpenAllCommand = ViewModelFactory.CreateViewModel<SimpleCommandViewModel.Factory>().Invoke(DataContext, this,
+                        ImportedFileNavigationSearchScreenViewModelResources.OpenAllCommand_Label,
+                        ImportedFileNavigationSearchScreenViewModelResources.OpenAllCommand_Tooltip,
+                        OpenAll, null, null);
+                    _OpenAllCommand.Icon = FrozenContext.FindPersistenceObject<Icon>(NamedObjects.Icon_fileopen_png);
+                }
+                return _OpenAllCommand;
+            }
+        }
+
+        public void OpenAll()
+        {
+            var newCtx = _ctxFactory();
+            var newWorkspace = ViewModelFactory.CreateViewModel<ObjectEditor.WorkspaceViewModel.Factory>().Invoke(newCtx, null);
+            ViewModelFactory.ShowModel(newWorkspace, true);
+
+            ViewModelFactory.CreateDelayedTask(newWorkspace, () =>
+            {
+                foreach (var obj in ListViewModel.Instances)
+                {
+                    newWorkspace.ShowForeignModel(obj);
+                }
+                newWorkspace.SelectedItem = newWorkspace.Items.FirstOrDefault();
+                newWorkspace.IsBusy = false;
+            }).Trigger();
         }
     }
 }
