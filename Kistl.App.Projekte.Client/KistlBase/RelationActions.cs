2fa67f140ea7ee59b0b2f168c56ae7f91373a127
Arthur Zaczek <arthur@dasz.at>
2011-11-22 15:03:16 +0100

    Workaround delete cascade troubles

--- Kistl.App.Projekte.Client/KistlBase/RelationActions.cs
+++ Kistl.App.Projekte.Client/KistlBase/RelationActions.cs
@@ -23,5 +23,26 @@ namespace Kistl.App.Base
             obj.A = obj.Context.Create<RelationEnd>();
             obj.B = obj.Context.Create<RelationEnd>();
         }
+
+        /// <summary>
+        /// Workaround delete cascade troubles
+        /// </summary>
+        /// <param name="obj"></param>
+        [Invocation]
+        public static void NotifyDeleting(Relation obj)
+        {
+            if (obj.A != null)
+            {
+                obj.A.Type = null;
+                obj.A.Navigator = null;
+                obj.Context.Delete(obj.A);
+            }
+            if (obj.B != null)
+            {
+                obj.B.Type = null;
+                obj.B.Navigator = null;
+                obj.Context.Delete(obj.B);
+            }
+        }
     }
 }
