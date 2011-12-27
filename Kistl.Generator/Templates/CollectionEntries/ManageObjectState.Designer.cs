f499da8c9f5901d96a06f1ba720e2533f6545abe
David Schmitt <david@dasz.at>
2011-12-27 09:34:53 +0100

    Do not try to undelete when not being Deleted

--- Kistl.Generator/Templates/CollectionEntries/ManageObjectState.Designer.cs
+++ Kistl.Generator/Templates/CollectionEntries/ManageObjectState.Designer.cs
@@ -31,14 +31,14 @@ namespace Kistl.Generator.Templates.CollectionEntries
         {
 #line 12 "P:\Kistl\Kistl.Generator\Templates\CollectionEntries\ManageObjectState.cst"
 this.WriteObjects("\r\n");
-this.WriteObjects("		protected override void OnPropertyChanged(string property, object oldValue, object newValue)\r\n");
+this.WriteObjects("        protected override void OnPropertyChanged(string property, object oldValue, object newValue)\r\n");
 this.WriteObjects("        {\r\n");
 this.WriteObjects("            base.OnPropertyChanged(property, oldValue, newValue);\r\n");
 this.WriteObjects("\r\n");
 this.WriteObjects("            if (property == \"A\" || property == \"B\")\r\n");
 this.WriteObjects("            {\r\n");
-this.WriteObjects("				var oldNotifier = (INotifyPropertyChanged)oldValue;\r\n");
-this.WriteObjects("				var newNotifier = (INotifyPropertyChanged)newValue;\r\n");
+this.WriteObjects("                var oldNotifier = (INotifyPropertyChanged)oldValue;\r\n");
+this.WriteObjects("                var newNotifier = (INotifyPropertyChanged)newValue;\r\n");
 this.WriteObjects("\r\n");
 this.WriteObjects("                if (oldNotifier != null) oldNotifier.PropertyChanged -= new System.ComponentModel.PropertyChangedEventHandler(AB_PropertyChanged);\r\n");
 this.WriteObjects("                if (newNotifier != null) newNotifier.PropertyChanged += new System.ComponentModel.PropertyChangedEventHandler(AB_PropertyChanged);\r\n");
@@ -61,9 +61,9 @@ this.WriteObjects("                this.Context.Delete(this);\r\n");
 this.WriteObjects("            if (B != null && B.ObjectState == DataObjectState.Deleted && this.Context != null)\r\n");
 this.WriteObjects("                this.Context.Delete(this);\r\n");
 this.WriteObjects("\r\n");
-this.WriteObjects("            if (A != null && B != null && A.ObjectState != DataObjectState.Deleted && B.ObjectState != DataObjectState.Deleted)\r\n");
+this.WriteObjects("            if (this.ObjectState == DataObjectState.Deleted && A != null && B != null && A.ObjectState != DataObjectState.Deleted && B.ObjectState != DataObjectState.Deleted)\r\n");
 this.WriteObjects("                this.SetUnDeleted();\r\n");
-this.WriteObjects("        }");
+this.WriteObjects("        }\r\n");
 
         }
 
