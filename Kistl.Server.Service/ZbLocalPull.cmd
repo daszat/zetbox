fc0ca5ae99dc3574071e93c646b5d58e448636bf
David Schmitt <david@dasz.at>
2012-03-08 10:54:57 +0100

    Do not pull Configs across projects

--- Kistl.Server.Service/ZbLocalPull.cmd
+++ Kistl.Server.Service/ZbLocalPull.cmd
@@ -26,6 +26,13 @@ IF ERRORLEVEL 1 GOTO FAIL
 rd /q /s %destination%\Tests
 IF ERRORLEVEL 1 GOTO FAIL
 
+rem since robocopy cannot exclude directories on the top-level only,
+rem we have to delete this after copying
+del /q /s /f %destination%\Configs
+IF ERRORLEVEL 1 GOTO FAIL
+rd /q /s %destination%\Configs
+IF ERRORLEVEL 1 GOTO FAIL
+
 robocopy %source%\Modules %destination%\Modules /MIR
 rem errorlevel 8 or higher indicates errors
 IF ERRORLEVEL 8 GOTO FAIL
