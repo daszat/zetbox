fb69f5c7e403d7bec6e087c8085cbf77077f7ecd
Arthur Zaczek <arthur@dasz.at>
2012-03-22 16:51:34 +0100

    Exclude more directories in ZbPull template

--- Templates/ProjectTemplates/KistlApp/KistlApp.Wizard/SolutionItems/ZbPull.cmd
+++ Templates/ProjectTemplates/KistlApp/KistlApp.Wizard/SolutionItems/ZbPull.cmd
@@ -10,22 +10,7 @@ if .%2. == .. GOTO FAIL
 set source=%1
 set destination=%2
 
-robocopy %source%\bin\Debug %destination% /MIR /XD Core.Generated NH.Generated EF.Generated
-rem errorlevel 8 or higher indicates errors
-IF ERRORLEVEL 8 GOTO FAIL
-
-rem since robocopy cannot exclude directories on the top-level only,
-rem we have to delete this after copying
-del /q /s /f %destination%\Tests
-IF ERRORLEVEL 1 GOTO FAIL
-rd /q /s %destination%\Tests
-IF ERRORLEVEL 1 GOTO FAIL
-
-robocopy %source%\Modules %destination%\Modules /MIR
-rem errorlevel 8 or higher indicates errors
-IF ERRORLEVEL 8 GOTO FAIL
-
-robocopy %source%\bin\Debug.HttpService %destination%\HttpService /MIR
+robocopy %source%\bin\Debug %destination% /MIR /XD Core.Generated NH.Generated EF.Generated DocumentStore Configs
 rem errorlevel 8 or higher indicates errors
 IF ERRORLEVEL 8 GOTO FAIL
 
