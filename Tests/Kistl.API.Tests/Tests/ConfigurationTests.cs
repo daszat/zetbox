191b3e48db8fa0390c443410b1a50705f2241bbf
David Schmitt <david@dasz.at>
2012-04-17 09:37:43 +0200

    Tests: arrays are not IEnumerable

--- Tests/Kistl.API.Tests/Tests/ConfigurationTests.cs
+++ Tests/Kistl.API.Tests/Tests/ConfigurationTests.cs
@@ -23,7 +23,6 @@ namespace Kistl.API.Tests
         {
             Assert.That(cfg.ConfigName, Is.Not.Empty, "ConfigName");
             Assert.That(cfg.AssemblySearchPaths, Is.Not.Null, "AssemblySearchPaths");
-            Assert.That(cfg.AssemblySearchPaths.Paths, Is.TypeOf<IEnumerable>(), "AssemblySearchPaths");
             Assert.That(cfg.AssemblySearchPaths.Paths, Is.Not.Empty, "AssemblySearchPaths");
 
             Assert.That(cfg.Client, Is.Not.Null, "Client");
