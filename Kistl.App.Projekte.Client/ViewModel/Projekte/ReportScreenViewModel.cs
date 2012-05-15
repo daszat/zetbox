e831e3421d2bd2f6ee36a9ccc98faddbaf02a936
Arthur Zaczek <arthur@dasz.at>
2012-05-15 15:25:16 +0200

    Tuning Statistic/Report example

--- Kistl.App.Projekte.Client/ViewModel/Projekte/ReportScreenViewModel.cs
+++ Kistl.App.Projekte.Client/ViewModel/Projekte/ReportScreenViewModel.cs
@@ -9,6 +9,7 @@ namespace Kistl.App.Projekte.Client.ViewModel.Projekte
     using Kistl.Client.Presentables;
     using Kistl.API;
     using Kistl.App.GUI;
+    using Kistl.API.Dtos;
 
     [ViewModelDescriptor]
     public class ReportScreenViewModel : NavigationReportScreenViewModel
@@ -21,14 +22,52 @@ namespace Kistl.App.Projekte.Client.ViewModel.Projekte
         {
         }
 
+        [Serializable]
+        [GuiPrintableRoot]
+        [GuiTitle("Projektbericht")]
         public class Report
         {
-            public string Name { get; set; }
+            public Report()
+            {
+                Months = new List<MonthEntry>();
+            }
+
+            //public DateTime From { get; set; }
+            //public DateTime Until { get; set; }
+
+            public decimal Total { get; set; }
+            public decimal TotalCorrected { get; set; }
+
+            [GuiTitle("Monate")]
+            public List<MonthEntry> Months { get; set; }
+
+            [Serializable]
+            public class MonthEntry
+            {
+                public string Year { get; set; }
+                public string Month { get; set; }
+
+                public decimal Total { get; set; }
+                public decimal TotalCorrected { get; set; }
+            }
         }
 
         protected override object LoadStatistic(DateTime from, DateTime until)
         {
-            return new Report() { Name = "Hello World" };
+            var rpt = new Report() { Total = 1, TotalCorrected = 2 };
+            rpt.Months.Add(new Report.MonthEntry() { Month = "01", Year = "2012", Total = 1, TotalCorrected = 10 });
+            rpt.Months.Add(new Report.MonthEntry() { Month = "02", Year = "2012", Total = 2, TotalCorrected = 10 });
+            rpt.Months.Add(new Report.MonthEntry() { Month = "03", Year = "2012", Total = 3, TotalCorrected = 10 });
+            rpt.Months.Add(new Report.MonthEntry() { Month = "04", Year = "2012", Total = 4, TotalCorrected = 10 });
+            rpt.Months.Add(new Report.MonthEntry() { Month = "05", Year = "2012", Total = 5, TotalCorrected = 10 });
+            rpt.Months.Add(new Report.MonthEntry() { Month = "06", Year = "2012", Total = 6, TotalCorrected = 10 });
+            rpt.Months.Add(new Report.MonthEntry() { Month = "07", Year = "2012", Total = 7, TotalCorrected = 10 });
+            rpt.Months.Add(new Report.MonthEntry() { Month = "08", Year = "2012", Total = 8, TotalCorrected = 10 });
+            rpt.Months.Add(new Report.MonthEntry() { Month = "09", Year = "2012", Total = 9, TotalCorrected = 10 });
+            rpt.Months.Add(new Report.MonthEntry() { Month = "00", Year = "2012", Total = 10, TotalCorrected = 10 });
+            rpt.Months.Add(new Report.MonthEntry() { Month = "11", Year = "2012", Total = 11, TotalCorrected = 10 });
+            rpt.Months.Add(new Report.MonthEntry() { Month = "12", Year = "2012", Total = 12, TotalCorrected = 10 });
+            return rpt;
         }
     }
 }
