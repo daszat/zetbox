040d838581b50394fbd9a15f95b71094d6827e9e
Arthur Zaczek <arthur@dasz.at>
2012-06-05 16:56:49 +0200

    deleting unused ClickOnce Generator

--- Kistl.Generator.ClickOnce/Properties/VersionInfo.cs
+++
@@ -1,5 +0,0 @@
-using System;
-using Kistl.API;
-
-// This shows the current branch, number of commits since the epoch, commit hash and whether(1) or not(0) the working directory is dirty
-[assembly: KistlSourceRevision("$BRANCH$-$REVNUM$-$REVID$-$DIRTY$")]
