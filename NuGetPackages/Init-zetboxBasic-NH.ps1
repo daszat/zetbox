6742e0d576181146780c0e095897f271a6878b68
Arthur Zaczek <arthur@dasz.at>
2012-06-14 12:15:31 +0200

    Using PrepareEnv for copy task during compilation of custom project

--- NuGetPackages/Init-zetboxBasic-NH.ps1
+++
@@ -1,6 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-
-$zetbox = "$installPath\..\..\.zetbox\"
-
-if (!(Test-Path -path $zetbox)) { mkdir $zetbox }
-cp "$toolsPath\*.targets" $zetbox
--- NuGetPackages/Init-zetboxBasic.ps1
+++ NuGetPackages/Init-zetboxBasic.ps1
@@ -1,6 +1,8 @@
 param($installPath, $toolsPath, $package, $project)
 
-$zetbox = "$installPath\..\..\.zetbox\"
+$zetbox = [System.IO.Path]::Combine($installPath, "..\..\.zetbox\")
 
 if (!(Test-Path -path $zetbox)) { mkdir $zetbox }
 cp "$toolsPath\*.targets" $zetbox
+cp "$toolsPath\Env.xml" $zetbox
+cp "$toolsPath\PrepareEnv.exe" $zetbox
