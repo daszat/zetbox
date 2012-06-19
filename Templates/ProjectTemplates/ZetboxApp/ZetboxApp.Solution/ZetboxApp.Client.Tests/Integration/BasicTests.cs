b21795df11d5c4867921674647523737a40a09b9
Arthur Zaczek <arthur@dasz.at>
2012-06-19 18:14:17 +0200

    Improving Template

--- Templates/ProjectTemplates/ZetboxApp/ZetboxApp.Solution/ZetboxApp.Client.Tests/Integration/BasicTests.cs
+++ Templates/ProjectTemplates/ZetboxApp/ZetboxApp.Solution/ZetboxApp.Client.Tests/Integration/BasicTests.cs
@@ -1,21 +1,21 @@
 
-namespace ZetboxApp.Solution.ZetboxApp.Client.Tests.Integration
+namespace $safeprojectname$.Integration
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using Zetbox.App.Base;
-    using NUnit.Framework;
+    //using System;
+    //using System.Collections.Generic;
+    //using System.Linq;
+    //using System.Text;
+    //using Zetbox.App.Base;
+    //using NUnit.Framework;
 
-    public class BasicTests : Zetbox.API.AbstractConsumerTests.AbstractTestFixture
-    {
-        [Test]
-        public void ConnectionTest()
-        {
-            var ctx = GetContext();
-            var result = ctx.GetQuery<ObjectClass>().FirstOrDefault();
-            Assert.That(result, Is.Not.Null);
-        }
-    }
+    //public class BasicTests : Zetbox.API.AbstractConsumerTests.AbstractTestFixture
+    //{
+    //    [Test]
+    //    public void ConnectionTest()
+    //    {
+    //        var ctx = GetContext();
+    //        var result = ctx.GetQuery<ObjectClass>().FirstOrDefault();
+    //        Assert.That(result, Is.Not.Null);
+    //    }
+    //}
 }
