0a617d876b6b804bd863463c279cc52e626b54d5
David Schmitt <david@dasz.at>
2012-06-28 12:39:58 +0200

    Templates: add SetUpFixture and enable BasicTests

--- Templates/ProjectTemplates/ZetboxApp/ZetboxApp.Solution/ZetboxApp.Client.Tests/Integration/BasicTests.cs
+++ Templates/ProjectTemplates/ZetboxApp/ZetboxApp.Solution/ZetboxApp.Client.Tests/Integration/BasicTests.cs
@@ -1,21 +1,21 @@
 
 namespace $safeprojectname$.Integration
 {
-    //using System;
-    //using System.Collections.Generic;
-    //using System.Linq;
-    //using System.Text;
-    //using Zetbox.App.Base;
-    //using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using NUnit.Framework;
+    using Zetbox.App.Base;
 
-    //public class BasicTests : Zetbox.API.AbstractConsumerTests.AbstractTestFixture
-    //{
-    //    [Test]
-    //    public void ConnectionTest()
-    //    {
-    //        var ctx = GetContext();
-    //        var result = ctx.GetQuery<ObjectClass>().FirstOrDefault();
-    //        Assert.That(result, Is.Not.Null);
-    //    }
-    //}
+    public class BasicTests : Zetbox.API.AbstractConsumerTests.AbstractTestFixture
+    {
+        [Test]
+        public void ConnectionTest()
+        {
+            var ctx = GetContext();
+            var result = ctx.GetQuery<ObjectClass>().FirstOrDefault();
+            Assert.That(result, Is.Not.Null);
+        }
+    }
 }
---
+++ Templates/ProjectTemplates/ZetboxApp/ZetboxApp.Solution/ZetboxApp.Client.Tests/SetUpFixture.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace $safeprojectname$
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Autofac;
+    using NUnit.Framework;
+    using Zetbox.API;
+
+    [SetUpFixture]
+    public class SetUpFixture : Zetbox.API.AbstractConsumerTests.AbstractSetUpFixture
+    {
+        protected override void SetupBuilder(ContainerBuilder builder)
+        {
+            base.SetupBuilder(builder);
+
+            // register local overrides here
+        }
+
+        protected override string GetConfigFile()
+        {
+            return "$safeprojectname$.xml";
+        }
+
+        protected override HostType GetHostType()
+        {
+            return HostType.Client;
+        }
+    }
+}
