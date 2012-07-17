a2c4d1ce1581ca9f0dcf2f089b409637e74ec7eb
David Schmitt <david@dasz.at>
2012-07-17 12:07:42 +0200

    Template: Automatically copy examples to local config, if necessary

--- Templates/ProjectTemplates/ZetboxApp/ZetboxApp.Wizard/SolutionItems/ZbInstall.cmd
+++ Templates/ProjectTemplates/ZetboxApp/ZetboxApp.Wizard/SolutionItems/ZbInstall.cmd
@@ -1,4 +1,5 @@
 @echo off
+IF NOT EXIST Configs\Local XCOPY /S/E Configs\Examples Configs\Local\
 .nuget\nuget.exe install .nuget\packages.config -o packages
 .zetbox\PrepareEnv.exe .zetbox
 echo Done
