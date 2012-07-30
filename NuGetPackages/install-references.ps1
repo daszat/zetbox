05c1141dc5efe7dd5841631f21ed7cfc58b91a52
David Schmitt <david@dasz.at>
2012-07-30 17:05:24 +0200

    NuGet: split DLLs into separate packages for proper referencing

---
+++ NuGetPackages/install-references.ps1
@@ -0,0 +1,23 @@
+﻿param($installPath, $toolsPath, $package, $project)
+# $installPath is the path to the folder where the package is installed
+# $toolsPath is the path to the tools directory in the folder where the package is installed
+# $package is a reference to the package object.
+# $project is a reference to the EnvDTE project object and represents the project the package is installed into.
+
+$ourReferences = (
+   "Zetbox.API",
+   "Zetbox.API.Client",
+   "Zetbox.API.Common",
+   "Zetbox.API.Server",
+   "Zetbox.Client",
+   "Zetbox.Client.WPF",
+   "Zetbox.Client.WPF.Toolkit"
+)
+
+foreach ($reference in $project.Object.References)
+{
+    if ($ourReferences -contains $reference.Name)
+    {
+        $reference.CopyLocal = $false;
+    }
+}
