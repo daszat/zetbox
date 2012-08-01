c82b2f0cf786b4d5ecdc9ce2034bcbe931d5522f
Arthur Zaczek <arthur@dasz.at>
2012-08-01 10:27:32 +0200

    Introduce BooleanToVisibilityHiddenConverter

--- Zetbox.Client.WPF.Toolkit/Converter/Converters.cs
+++ Zetbox.Client.WPF.Toolkit/Converter/Converters.cs
@@ -365,4 +365,23 @@ namespace Zetbox.Client.WPF.Converter
             return Binding.DoNothing;
         }
     }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [ValueConversion(typeof(bool), typeof(System.Windows.Visibility))]
+    public class BooleanToVisibilityHiddenConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+                            object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value is bool && (bool)value ? System.Windows.Visibility.Visible : System.Windows.Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+                            object parameter, System.Globalization.CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
 }
