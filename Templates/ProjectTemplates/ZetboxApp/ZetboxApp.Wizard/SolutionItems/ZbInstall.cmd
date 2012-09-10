23dfa44246bfdd5696823cd9696a5c2102545435
David Schmitt <david@dasz.at>
2012-09-10 13:11:15 +0200

    Templates: fix zbinstall to use all available packages.configs

--- Templates/ProjectTemplates/ZetboxApp/ZetboxApp.Wizard/SolutionItems/ZbInstall.cmd
+++ Templates/ProjectTemplates/ZetboxApp/ZetboxApp.Wizard/SolutionItems/ZbInstall.cmd
@@ -1,5 +1,5 @@
 @echo off
 IF NOT EXIST Configs\Local XCOPY /S/E Configs\Examples Configs\Local\
-.nuget\nuget.exe install .nuget\packages.config -o packages
+FOR /R %%G IN (packages.config) DO IF EXIST %%G .nuget\nuget.exe install %%G -o packages
 .zetbox\PrepareEnv.exe .zetbox
 echo Done
