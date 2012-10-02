f2563ef23fded276a8296f320a924e205501f57f
Arthur Zaczek <arthur@dasz.at>
2012-10-02 20:18:11 +0200

    fix gendarme error

--- Zetbox.Client.WPF/View/DocumentManagement/PreviewersManager.cs
+++ Zetbox.Client.WPF/View/DocumentManagement/PreviewersManager.cs
@@ -208,18 +208,6 @@ namespace Zetbox.Client.WPF.View.DocumentManagement
         void Initialize(IShellItem pstream, uint grfMode);
     }
 
-    enum SIGDN : uint
-    {
-        NORMALDISPLAY = 0,
-        PARENTRELATIVEPARSING = 0x80018001,
-        PARENTRELATIVEFORADDRESSBAR = 0x8001c001,
-        DESKTOPABSOLUTEPARSING = 0x80028000,
-        PARENTRELATIVEEDITING = 0x80031001,
-        DESKTOPABSOLUTEEDITING = 0x8004c000,
-        FILESYSPATH = 0x80058000,
-        URL = 0x80068000
-    }
-
     [ComImport]
     [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
     [Guid("43826d1e-e718-42ee-bc55-a1e261c37bfe")]
@@ -232,7 +220,7 @@ namespace Zetbox.Client.WPF.View.DocumentManagement
 
         void GetParent(out IShellItem ppsi);
 
-        void GetDisplayName(SIGDN sigdnName, out IntPtr ppszName);
+        void GetDisplayName(uint sigdnName, out IntPtr ppszName);
 
         void GetAttributes(uint sfgaoMask, out uint psfgaoAttribs);
 
