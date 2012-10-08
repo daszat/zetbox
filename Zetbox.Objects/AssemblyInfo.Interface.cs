0f4e0f332ecae4207b722631baa2c5a3c4249d21
Arthur Zaczek <arthur@dasz.at>
2012-10-08 16:13:40 +0200

    Upgrade Assembly Versions in database

--- Zetbox.Objects/AssemblyInfo.Interface.cs
+++ Zetbox.Objects/AssemblyInfo.Interface.cs
@@ -9,4 +9,4 @@
 [assembly: System.Runtime.InteropServices.ComVisibleAttribute(false)]
 // [assembly: System.CLSCompliantAttribute(true)] // TODO: needs PositionSuffix without underscore and naming columns differently
 [assembly: System.CLSCompliantAttribute(true)]
-[assembly: Zetbox.API.ZetboxGeneratedVersion("f9585b8b-a85a-4b09-8f40-02d058a357c3")]
+[assembly: Zetbox.API.ZetboxGeneratedVersion("4802dd08-efbc-47e9-93b9-3b12d267470f")]
