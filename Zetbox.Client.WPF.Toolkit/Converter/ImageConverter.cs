226d2ee26e27f6e1dfbb4edceb161686f9f089e2
Arthur Zaczek <arthur@dasz.at>
2012-11-14 14:53:37 +0100

    Protect image converter with try/catch

--- Zetbox.Client.WPF.Toolkit/Converter/ImageConverter.cs
+++ Zetbox.Client.WPF.Toolkit/Converter/ImageConverter.cs
@@ -22,38 +22,47 @@ namespace Zetbox.Client.WPF.Converter
     using System.Windows.Media.Imaging;
     using System.Globalization;
     using System.IO;
+    using Zetbox.API.Utils;
     
     public sealed class ImageConverter : IValueConverter
     {
         public object Convert(object value, Type targetType,
                               object parameter, CultureInfo culture)
         {
-            if (value is string)
+            try
             {
-                return new BitmapImage(new Uri((string)value));
+                if (value is string)
+                {
+                    return new BitmapImage(new Uri((string)value));
+                }
+                else if (value is System.IO.Stream)
+                {
+                    var bmp = new BitmapImage();
+                    bmp.BeginInit();
+                    bmp.StreamSource = (System.IO.Stream)value;
+                    bmp.EndInit();
+                    return bmp;
+                }
+                else if (value is System.Drawing.Image)
+                {
+                    var bmp = new BitmapImage();
+                    bmp.BeginInit();
+                    var ms = new MemoryStream();
+                    var img = (System.Drawing.Image)value;
+                    img.Save(ms, img.RawFormat);
+                    ms.Seek(0, SeekOrigin.Begin);
+                    bmp.StreamSource = ms;
+                    bmp.EndInit();
+                    return bmp;
+                }
+                else
+                {
+                    return Binding.DoNothing;
+                }
             }
-            else if (value is System.IO.Stream)
+            catch (Exception ex)
             {
-                var bmp = new BitmapImage();
-                bmp.BeginInit();
-                bmp.StreamSource = (System.IO.Stream)value;
-                bmp.EndInit();
-                return bmp;
-            }
-            else if (value is System.Drawing.Image)
-            {
-                var bmp = new BitmapImage();
-                bmp.BeginInit();
-                var ms = new MemoryStream();
-                var img = (System.Drawing.Image)value;
-                img.Save(ms, img.RawFormat);
-                ms.Seek(0, SeekOrigin.Begin);
-                bmp.StreamSource = ms;
-                bmp.EndInit();
-                return bmp;
-            }
-            else
-            {
+                Logging.Log.Info("Error while converting image", ex);
                 return Binding.DoNothing;
             }
         }
