a7e88e19940af3e379e6218d157cb1669845d925
Arthur Zaczek <arthur@dasz.at>
2012-12-07 16:22:30 +0100

    Adding service installer

--- Zetbox.Server.Service/WindowsService.Designer.cs
+++ Zetbox.Server.Service/WindowsService.Designer.cs
@@ -28,8 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            this.ServiceName = "WindowsService";
+            // 
+            // WindowsService
+            // 
+            this.ServiceName = "zetbox Service";
+
         }
 
         #endregion
