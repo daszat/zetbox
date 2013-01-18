6b317e6220216776d6a50a10da980d9646a869d7
Arthur Zaczek <arthur@dasz.at>
2013-01-18 14:17:55 +0100

    Template: quote paths in ZbInstall.cmd

--- Templates/ProjectTemplates/ZetboxApp/ZetboxApp.Wizard/SolutionItems/ZbInstall.cmd
+++ Templates/ProjectTemplates/ZetboxApp/ZetboxApp.Wizard/SolutionItems/ZbInstall.cmd
@@ -1,5 +1,5 @@
 @echo off
 IF NOT EXIST Configs\Local XCOPY /S/E Configs\Examples Configs\Local\
-FOR /R %%G IN (packages.config) DO IF EXIST %%G .nuget\nuget.exe install %%G -o packages
+FOR /R %%G IN (packages.config) DO IF EXIST "%%G" .nuget\nuget.exe install "%%G" -o packages
 .zetbox\PrepareEnv.exe .zetbox
 echo Done
