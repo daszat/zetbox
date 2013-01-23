d01321918c5fe595880b47a97c14ff036f533db0
David Schmitt <david@dasz.at>
2013-01-23 10:50:11 +0100

    ConfigEditor: use Mono.Cecil to load assemblies
    This is much faster, doesn't throw oodles of exceptions and is generally
    much nicer.

--- Zetbox.ConfigEditor/ModulesCache.cs
+++ Zetbox.ConfigEditor/ModulesCache.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Reflection;
-using System.IO;
-
+﻿
 namespace Zetbox.ConfigEditor
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using Mono.Cecil;
+    using Zetbox.API;
+
     public class ModuleType
     {
-        public Type Type { get; set; }
+        public TypeDefinition Type { get; set; }
         public string TypeName { get; set; }
         public string Description { get; set; }
         public bool IsFeature { get; set; }
@@ -20,18 +22,6 @@ namespace Zetbox.ConfigEditor
     {
         private ModulesCache()
         {
-            // ReflectionOnlyLoadFrom.GetTypes -> dependent assemblies must be pre-loaded or loaded on demand through the ReflectionOnlyAssemblyResolve event
-            AppDomain.CurrentDomain.ReflectionOnlyAssemblyResolve += new ResolveEventHandler(CurrentDomain_ReflectionOnlyAssemblyResolve);
-        }
-
-        private Assembly CurrentDomain_ReflectionOnlyAssemblyResolve(object sender, ResolveEventArgs args)
-        {
-            var aName = new AssemblyName(args.Name);
-            var file = Directory.GetFiles(Environment.CurrentDirectory, aName.Name + ".dll", SearchOption.AllDirectories).FirstOrDefault();
-            if (file != null)
-                return System.Reflection.Assembly.ReflectionOnlyLoadFrom(file);
-            else
-                return System.Reflection.Assembly.ReflectionOnlyLoad(args.Name);
         }
 
         private static ModulesCache _instance;
@@ -52,8 +42,8 @@ namespace Zetbox.ConfigEditor
             get
             {
                 EnsureCache();
-                if (_modules.ContainsKey(typeName))
-                    return _modules[typeName];
+                if (_autofacModules.ContainsKey(typeName))
+                    return _autofacModules[typeName];
                 else
                     return new ModuleType() { TypeName = typeName };
             }
@@ -62,7 +52,7 @@ namespace Zetbox.ConfigEditor
         public bool Contains(string typeName)
         {
             EnsureCache();
-            return _modules.ContainsKey(typeName);
+            return _autofacModules.ContainsKey(typeName);
         }
 
         public IEnumerable<ModuleType> All
@@ -70,19 +60,19 @@ namespace Zetbox.ConfigEditor
             get
             {
                 EnsureCache();
-                return _modules.Values;
+                return _autofacModules.Values;
             }
         }
 
-        private Dictionary<string, Assembly> _assemblies;
-        private Dictionary<string, ModuleType> _modules;
+        private Dictionary<string, ModuleDefinition> _modules;
+        private Dictionary<string, ModuleType> _autofacModules;
 
         private void EnsureCache()
         {
-            if (_assemblies == null)
+            if (_modules == null)
             {
-                _assemblies = new Dictionary<string, Assembly>();
-                _modules = new Dictionary<string, ModuleType>();
+                _modules = new Dictionary<string, ModuleDefinition>();
+                _autofacModules = new Dictionary<string, ModuleType>();
                 foreach (var file in Directory.GetFiles(Environment.CurrentDirectory, "*.dll", SearchOption.AllDirectories))
                 {
                     if (file.ToLowerInvariant().EndsWith(".resources.dll")) continue;
@@ -100,113 +90,61 @@ namespace Zetbox.ConfigEditor
 
         private void LoadAssembly(string file)
         {
-            if (_assemblies.ContainsKey(FileToAssemblyName(file))) return;
+            var cacheKey = FileToCacheKey(file);
+            if (_modules.ContainsKey(cacheKey)) return;
 
-            Assembly assembly = TryLoadAssembly(file);
-            if (assembly == null) return;
+            var module = _modules[cacheKey] = ModuleDefinition.ReadModule(file);
 
-            var assemblyName = assembly.GetName().Name;
-            _assemblies[assemblyName] = assembly;
-
-            foreach (var type in GetTypes(assembly))
+            foreach (var type in module.Types.SelectMany(t => t.AndChildren(p => p.NestedTypes)))
             {
-                if (type == null) continue; // Oh, yes!
-
                 if (type.BaseType != null && type.BaseType.FullName == "Autofac.Module") // By string, as we do not want to rely on a specific autofac version
                 {
                     var mt = new ModuleType()
                     {
                         Type = type,
-                        TypeName = string.Format("{0}, {1}", type.FullName, assemblyName),
+                        TypeName = string.Format("{0}, {1}", type.FullName.Replace('/', '+'), module.Assembly.Name.Name),
                         Description = ExtractDescription(type),
                         IsFeature = ExtractIsFeature(type),
                         NotOnFallback = ExtractNotOnFallback(type)
                     };
-                    _modules[mt.TypeName] = mt;
+                    _autofacModules[mt.TypeName] = mt;
                 }
             }
         }
 
-        private static string ExtractDescription(Type type)
+        private static string ExtractDescription(TypeDefinition type)
         {
-            string description = string.Empty;
-            var attr = CustomAttributeData.GetCustomAttributes(type).FirstOrDefault(i => i.Constructor.DeclaringType.FullName == typeof(System.ComponentModel.DescriptionAttribute).FullName);
-            if (attr != null)
+            var attr = type.CustomAttributes.FirstOrDefault(i => i.AttributeType.FullName == typeof(System.ComponentModel.DescriptionAttribute).FullName);
+
+            if (attr != null && attr.ConstructorArguments.Count > 0)
             {
-                var namedArg = attr.NamedArguments.FirstOrDefault(i => i.MemberInfo.Name == "Description");
-                if (namedArg != null && namedArg.TypedValue.Value != null)
-                {
-                    description = (string)namedArg.TypedValue.Value;
-                }
-                else if (attr.ConstructorArguments.Count > 0)
-                {
-                    description = (string)attr.ConstructorArguments.First().Value;
-                }
+                return (string)attr.ConstructorArguments.First().Value;
             }
-            return description;
+            return string.Empty;
         }
 
-        private static bool ExtractIsFeature(Type type)
+        private static bool ExtractIsFeature(TypeDefinition type)
         {
-            var attr = CustomAttributeData.GetCustomAttributes(type).FirstOrDefault(i => i.Constructor.DeclaringType.FullName == typeof(Zetbox.API.Configuration.FeatureAttribute).FullName);
+            var attr = type.CustomAttributes.FirstOrDefault(i => i.AttributeType.FullName == typeof(Zetbox.API.Configuration.FeatureAttribute).FullName);
             return attr != null;
         }
 
-        private static bool ExtractNotOnFallback(Type type)
+        private static bool ExtractNotOnFallback(TypeDefinition type)
         {
             bool notOnFallback = false;
-            var attr = CustomAttributeData.GetCustomAttributes(type).FirstOrDefault(i => i.Constructor.DeclaringType.FullName == typeof(Zetbox.API.Configuration.FeatureAttribute).FullName);
+            var attr = type.CustomAttributes.FirstOrDefault(i => i.AttributeType.FullName == typeof(Zetbox.API.Configuration.FeatureAttribute).FullName);
             if (attr != null)
             {
-                var namedArg = attr.NamedArguments.FirstOrDefault(i => i.MemberInfo.Name == "NotOnFallback");
-                if (namedArg != null)
+                var namedArg = attr.Properties.Where(i => i.Name == "NotOnFallback").ToArray();
+                if (namedArg.Length > 0)
                 {
-                    notOnFallback = namedArg.TypedValue.Value as bool? == true;
+                    notOnFallback = namedArg[0].Argument.Value as bool? == true;
                 }
             }
             return notOnFallback;
         }
 
-        private static Type[] GetTypes(Assembly assembly)
-        {
-            Type[] types;
-            try
-            {
-                types = assembly.GetTypes();
-            }
-            catch (ReflectionTypeLoadException ex)
-            {
-                // These where all types without an error - we are a simple tool, we can ignore all other load errors
-                types = ex.Types;
-            }
-            return types;
-        }
-
-        private Assembly TryLoadAssembly(string file)
-        {
-            var assemblyName = FileToAssemblyName(file);
-            if (_assemblies.ContainsKey(assemblyName))
-            {
-                return _assemblies[assemblyName];
-            }
-
-            Assembly assembly = null;
-            try
-            {
-                assembly = Assembly.ReflectionOnlyLoad(assemblyName);
-            }
-            catch (FileNotFoundException)
-            {
-            }
-
-            if (assembly == null)
-            {
-                assembly = Assembly.ReflectionOnlyLoadFrom(file);
-            }
-            return assembly;
-        }
-
-        private static string FileToAssemblyName(string file)
+        private static string FileToCacheKey(string file)
         {
             return Path.GetFileNameWithoutExtension(file);
         }
