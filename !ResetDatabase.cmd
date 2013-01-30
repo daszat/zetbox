8ddaab654188b9fe4a85c3761915f97440679dd6
Arthur Zaczek <arthur@dasz.at>
2013-01-30 10:01:53 +0100

    when resetting database, also re-sync identities

--- !ResetDatabase.cmd
+++ !ResetDatabase.cmd
@@ -12,7 +12,7 @@ set config=%1
 
 :GOON
 
-bin\debug\Zetbox.Cli.exe %config% -fallback -wipe -deploy-update -generate
+bin\debug\Zetbox.Cli.exe %config% -fallback -wipe -deploy-update -generate -syncidentities
 IF ERRORLEVEL 1 GOTO FAIL
 
 echo ********************************************************************************
