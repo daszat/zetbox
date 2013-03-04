9a50f0cfc2bc0033278a10bfcafd8077d928e836
Arthur Zaczek <arthur@dasz.at>
2013-03-04 11:43:52 +0100

    NExtEventsSummary: Setting up test infrastructure

--- Zetbox.Client/Presentables/TestModule/NextEventsTestNavScreenViewModel.cs
+++ Zetbox.Client/Presentables/TestModule/NextEventsTestNavScreenViewModel.cs
@@ -8,6 +8,7 @@ namespace Zetbox.Client.Presentables.TestModule
     using Zetbox.Client.Presentables;
     using Zetbox.Client.Presentables.GUI;
     using Zetbox.App.GUI;
+using Zetbox.Client.Presentables.Calendar;
 
     [ViewModelDescriptor]
     public class NextEventsTestNavScreenViewModel : NavigationScreenViewModel
@@ -21,6 +22,19 @@ namespace Zetbox.Client.Presentables.TestModule
             : base(appCtx, dataCtx, parent, screen)
         {
             _ctxFactory = ctxFactory;
+        }
+
+        private NextEventsSummaryViewModel _nextEvents;
+        public NextEventsSummaryViewModel NextEvents
+        {
+            get
+            {
+                if (_nextEvents == null)
+                {
+                    _nextEvents = ViewModelFactory.CreateViewModel<NextEventsSummaryViewModel.Factory>().Invoke(DataContext, this);
+                }
+                return _nextEvents;
+            }
         }
     }
 }
