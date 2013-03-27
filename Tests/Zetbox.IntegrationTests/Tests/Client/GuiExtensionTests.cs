1063b41457f111435fd3b227e9543004855d4543
David Schmitt <david@dasz.at>
2013-03-27 11:31:37 +0100

    Tests: Add smoke screening tests for Client

---
+++ Tests/Zetbox.IntegrationTests/Tests/Client/GuiExtensionTests.cs
@@ -0,0 +1,42 @@
+﻿// This file is part of zetbox.
+//
+// Zetbox is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
+//
+// Zetbox is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with zetbox.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace Zetbox.IntegrationTests.Client
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Autofac;
+    using NUnit.Framework;
+    using Zetbox.API.AbstractConsumerTests;
+    using Zetbox.App.Extensions;
+    using Zetbox.App.GUI;
+    using Zetbox.Client.Presentables;
+
+    public class GuiExtensionTests : AbstractTestFixture
+    {
+        [Test]
+        public void should_resolve_ViewDescriptor()
+        {
+            var ctx = GetContext();
+            var vmd = ctx.GetQuery<ViewModelDescriptor>().First(v => v.ViewModelTypeRef == "Zetbox.Client.Presentables.DataObjectViewModel, Zetbox.Client");
+            var vd = vmd.GetViewDescriptor(Toolkit.WPF);
+
+            Assert.That(vd, Is.Not.Null);
+            Assert.That(vd.ControlTypeRef, Is.EqualTo("Zetbox.Client.WPF.View.ObjectEditor.DataObjectEditor, Zetbox.Client.WPF"));
+        }
+    }
+}
