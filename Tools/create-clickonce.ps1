05960377e84b80754bf316db5b4e7b62e66f4207
Arthur Zaczek <arthur@dasz.at>
2013-09-20 15:22:42 +0200

    improve create click-once deployment script

--- Tools/create-clickonce.ps1
+++ Tools/create-clickonce.ps1
@@ -15,35 +15,66 @@ Param(
 	[string]$DBUrl = "http://demo.dasz.at/zetbox", 
 	[string]$Version = "1.0.0.0",
 	[string]$Publisher = "dasz.at OG",
-	[string]$ProviderUrl = "http://dasz.at/foobar",
+	[string]$ProviderUrl = "http://demo.dasz.at/zetbox/client",
 	[string]$SourcePath = "..\bin\Debug\Bootstrapper",
 	[string]$CertFile = (get-location).Path + "\tempcert"
 )
+$ErrorActionPreference = "Stop"
+
+# http://rkeithhill.wordpress.com/2009/08/03/effective-powershell-item-16-dealing-with-errors/
+function CheckLastExitCode {
+    param ([int[]]$SuccessCodes = @(0), [scriptblock]$CleanupScript=$null)
+
+    if ($SuccessCodes -notcontains $LastExitCode) {
+        if ($CleanupScript) {
+            "Executing cleanup script: $CleanupScript"
+            &$CleanupScript
+        }
+        $msg = @"
+EXE RETURNED EXIT CODE $LastExitCode
+CALLSTACK:$(Get-PSCallStack | Out-String)
+"@
+        throw $msg
+    }
+}
+
 
 $assembly = "Zetbox.Client.Bootstrapper"
-$out_path = ".\publish\"
+$outPath = (get-location).Path + "\publish\"
 
-$mage_path="${env:ProgramFiles(x86)}\Microsoft SDKs\Windows\v7.0A\Bin\NETFX 4.0 Tools\mage.exe"
+$magePath = "${env:ProgramFiles(x86)}\Microsoft SDKs\Windows\v7.0A\Bin\NETFX 4.0 Tools\mage.exe"
 
-if (!(Test-Path -path "$out_path\$Version")) {
-	mkdir "$out_path\$Version" | out-null
+if (!(Test-Path -path "$outPath\$Version")) {
+	mkdir "$outPath\$Version" | out-null
 }	
 
-cp -Recurse "$SourcePath\*.*" "$out_path\$Version" -exclude "*.pdb" -force
+cp -Recurse "$SourcePath\*.*" "$outPath\$Version" -exclude "*.pdb" -force
+
+$configFile = "$outPath\$Version\$assembly.exe.config"
+[xml]$cfgXML = Get-Content $configFile
+$addressTag = ($cfgXML.Configuration.userSettings.{Zetbox.Client.Bootstrapper.Properties.Settings}.setting | ? { $_.name -eq "Address" })
+$addressTag.value = $DBUrl
+$cfgXML.Save($configFile)
 
 if (!(Test-Path "$CertFile.pfx")) {
-	"Creating tempcert" | out-host
+	"Creating temp cert" | out-host
 	makecert -sv "$CertFile.pvk" -n "CN=Temp-Cert" "$CertFile.cer"
 	pvk2pfx -pvk "$CertFile.pvk" -spc "$CertFile.cer" -pfx "$CertFile.pfx"
 }
 
 Push-Location
-cd "$out_path\$Version\"
-&($mage_path) -New Application -Processor msil -ToFile "$assembly.exe.manifest" -name "$Name" -Version "$Version" -FromDirectory . 
-&($mage_path) -Sign "$assembly.exe.manifest" -CertFile "$CertFile.pfx"
+try {
+	cd "$outPath\$Version\"
+	&($magePath) -New Application -Processor msil -ToFile "$assembly.exe.manifest" -name "$Name" -Version "$Version" -FromDirectory . 
+	CheckLastExitCode
+	&($magePath) -Sign "$assembly.exe.manifest" -CertFile "$CertFile.pfx"
+	CheckLastExitCode
 
-cd ..
-&($mage_path) -New Deployment -Processor msil -Version "$Version" -Install true -Publisher "$Publisher" -ProviderUrl "$ProviderUrl" -AppManifest "$Version\$assembly.exe.manifest" -ToFile "$Name.application"
-&($mage_path) -Sign "$Name.application" -CertFile "$CertFile.pfx"
-
-Pop-Location
+	cd .. 
+	&($magePath) -New Deployment -Processor msil -Version "$Version" -Install true -Publisher "$Publisher" -ProviderUrl "$ProviderUrl" -AppManifest "$Version\$assembly.exe.manifest" -ToFile "$Name.application"
+	CheckLastExitCode
+	&($magePath) -Sign "$Name.application" -CertFile "$CertFile.pfx"
+	CheckLastExitCode
+} finally {
+	Pop-Location
+}
