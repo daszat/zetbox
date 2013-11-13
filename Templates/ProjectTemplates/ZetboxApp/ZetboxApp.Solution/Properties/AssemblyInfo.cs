45f9681bb0e447f9df4bd7a1b6b84769f53840ce
Arthur Zaczek <arthur@dasz.at>
2013-11-13 15:36:56 +0100

    Template: Reverting version numbers

--- Templates/ProjectTemplates/ZetboxApp/ZetboxApp.Solution/Properties/AssemblyInfo.cs
+++ Templates/ProjectTemplates/ZetboxApp/ZetboxApp.Solution/Properties/AssemblyInfo.cs
@@ -22,15 +22,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("faf10ad5-be6f-4d8f-aa64-3259c0e5349f")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
