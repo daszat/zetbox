6af6ee72eb0434dcda371c229e38a12cef63e9c1
Arthur Zaczek <arthur@dasz.at>
2013-12-10 10:15:26 +0100

    Template: Add nuget package cleanup command to zbinstall

--- Templates/ProjectTemplates/ZetboxApp/ZetboxApp.Wizard/SolutionItems/ZbInstall.cmd
+++ Templates/ProjectTemplates/ZetboxApp/ZetboxApp.Wizard/SolutionItems/ZbInstall.cmd
@@ -1,5 +1,9 @@
 @echo off
 IF NOT EXIST Configs\Local XCOPY /S/E Configs\Examples Configs\Local\
+
+rem nuget install does not remove old versions
+rmdir /q /s .\packages
+
 FOR /R %%G IN (packages.config) DO IF EXIST "%%G" .nuget\nuget.exe install "%%G" -o packages
 .zetbox\PrepareEnv.exe .zetbox
 echo Done
