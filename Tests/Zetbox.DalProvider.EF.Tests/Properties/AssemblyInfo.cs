cb43d15f9d123d33ed2e631d66e6e72b4a406477
Jenkins <jenkins@dasz.at>
2014-01-08 14:00:21 +0100

    Update version number for next development cycle: 1.1.6-zdev

--- Tests/Zetbox.DalProvider.EF.Tests/Properties/AssemblyInfo.cs
+++ Tests/Zetbox.DalProvider.EF.Tests/Properties/AssemblyInfo.cs
@@ -35,7 +35,7 @@ using System.Runtime.InteropServices;
 
 // The following tags will be replaced by the -checkout job on jenkins to current values
 [assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.5.0")]
-[assembly: AssemblyInformationalVersion("1.1.5-zdev")]
+[assembly: AssemblyFileVersion("1.1.6.0")]
+[assembly: AssemblyInformationalVersion("1.1.6-zdev")]
 
 [assembly: System.Data.Objects.DataClasses.EdmSchemaAttribute()]
