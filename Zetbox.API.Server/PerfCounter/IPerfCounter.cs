955e897fb45d52a3404c125a3b70c898fea02848
David Schmitt <david@dasz.at>
2014-10-20 19:38:28 +0200

    Add stacktrace logging to PerfCounter's LifetimeScopeExtensions
    If Zetbox.Server.PerfCounter.LifetimeScopeExtensions log level is set to
    debug, the allocation stack traces of the first 100 remaining
    LifetimeScopes are logged.

--- Zetbox.API.Server/PerfCounter/IPerfCounter.cs
+++ Zetbox.API.Server/PerfCounter/IPerfCounter.cs
@@ -15,6 +15,7 @@
 namespace Zetbox.API.Server.PerfCounter
 {
     using System;
+    using System.Collections.Generic;
     using Autofac;
     using Zetbox.API.PerfCounter;
 
@@ -28,9 +29,50 @@ namespace Zetbox.API.Server.PerfCounter
 
     public static class LifetimeScopeExtensions
     {
+        private readonly static log4net.ILog Log = log4net.LogManager.GetLogger("Zetbox.Server.PerfCounter.LifetimeScopeExtensions");
+        private static readonly object _allocationTraceLock = new object();
+        private static readonly List<object> allocationTraces = new List<object>();
+        private static int debuggedTraces = 0;
+
         public static void ApplyPerfCounterTracker(this ILifetimeScope scope)
         {
-            scope.ChildLifetimeScopeBeginning += (s, a) => a.LifetimeScope.ApplyPerfCounterTracker();
+            scope.ChildLifetimeScopeBeginning += (sender, a) =>
+            {
+                // recurse down the scope tree
+                a.LifetimeScope.ApplyPerfCounterTracker();
+
+                // record stack traces of currently active scopes
+                string trace = string.Empty;
+                if (Log.IsDebugEnabled) trace = Environment.StackTrace;
+
+                lock (_allocationTraceLock)
+                {
+                    allocationTraces.Add(trace);
+                }
+                a.LifetimeScope.CurrentScopeEnding += (s, e) =>
+                {
+                    lock (_allocationTraceLock)
+                    {
+                        allocationTraces.Remove(trace);
+                        Log.DebugFormat("CurrentScopeEnding: {0} LifetimeScopes remain", allocationTraces.Count);
+                        if (Log.IsDebugEnabled && allocationTraces.Count > 10 && debuggedTraces < 100)
+                        {
+                            foreach (var t in allocationTraces)
+                            {
+                                Log.Debug("TRACE:");
+                                Log.Debug(t);
+                                Log.Debug("=========================================");
+
+                                debuggedTraces += 1;
+                                if (debuggedTraces > 100)
+                                    break;
+                            }
+                        }
+                    }
+                };
+            };
+
+            // perfCtr measurements
             var perfCtr = scope.Resolve<IPerfCounter>();
             var startTicks = perfCtr.IncrementLifetimeScope();
             scope.CurrentScopeEnding += (s, a) => perfCtr.DecrementLifetimeScope(startTicks);
