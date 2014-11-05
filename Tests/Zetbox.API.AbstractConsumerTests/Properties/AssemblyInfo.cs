4da6378b8de757aea88c07a2b49b8a1ceeef98d9
Jenkins <jenkins@dasz.at>
2014-11-05 15:24:09 +0100

    Update version number for next development cycle: 1.1.15-zdev

--- Tests/Zetbox.API.AbstractConsumerTests/Properties/AssemblyInfo.cs
+++ Tests/Zetbox.API.AbstractConsumerTests/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 
 // The following tags will be replaced by the -checkout job on jenkins to current values
 [assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.14.0")]
-[assembly: AssemblyInformationalVersion("1.1.14-zdev")]
+[assembly: AssemblyFileVersion("1.1.15.0")]
+[assembly: AssemblyInformationalVersion("1.1.15-zdev")]
