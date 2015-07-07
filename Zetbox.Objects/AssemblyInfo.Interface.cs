e9d0f0a8fd78586e007fd2fbdd0c9834354963dc
Arthur Zaczek <zaczek@technikum-wien.at>
2015-07-07 15:42:03 +0200

    Export modules with new relation names

--- Zetbox.Objects/AssemblyInfo.Interface.cs
+++ Zetbox.Objects/AssemblyInfo.Interface.cs
@@ -9,4 +9,4 @@
 [assembly: System.Runtime.InteropServices.ComVisibleAttribute(false)]
 // [assembly: System.CLSCompliantAttribute(true)] // TODO: needs PositionSuffix without underscore and naming columns differently
 [assembly: System.CLSCompliantAttribute(true)]
-[assembly: Zetbox.API.ZetboxGeneratedVersion("0d3e6934-76ad-4d9b-ab76-bf5ce11b5f9e")]
+[assembly: Zetbox.API.ZetboxGeneratedVersion("eceabfe9-0041-4089-b198-d7ba72144f94")]
