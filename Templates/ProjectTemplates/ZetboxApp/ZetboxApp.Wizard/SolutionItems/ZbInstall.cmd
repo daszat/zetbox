4e8945925ccac4426caa4fb7b3ef02afafa6d1ce
Arthur Zaczek <arthur@dasz.at>
2017-07-31 13:55:31 +0200

    nuget should resist on the path. Reflect that in the project template

--- Templates/ProjectTemplates/ZetboxApp/ZetboxApp.Wizard/SolutionItems/ZbInstall.cmd
+++ Templates/ProjectTemplates/ZetboxApp/ZetboxApp.Wizard/SolutionItems/ZbInstall.cmd
@@ -4,6 +4,6 @@ IF NOT EXIST Configs\Local XCOPY /S/E Configs\Examples Configs\Local\
 rem nuget install does not remove old versions
 rmdir /q /s .\packages
 
-FOR /R %%G IN (packages.config) DO IF EXIST "%%G" .nuget\nuget.exe install "%%G" -o packages
+FOR /R %%G IN (packages.config) DO IF EXIST "%%G" nuget install "%%G" -o packages
 .zetbox\PrepareEnv.exe .zetbox
 echo Done
