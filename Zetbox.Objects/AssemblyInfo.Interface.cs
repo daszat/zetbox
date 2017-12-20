a0feb586ca654407b2bdf1222875152e906d9db1
Arthur Zaczek <arthur@dasz.at>
2017-12-20 16:40:45 +0100

    #20: Filterconfiguration: Fix Summary only (without Main) property categories, #19: Icons: Add IconFile to default search, #18: ModuleEditor: All properties list: add a filter for name

--- Zetbox.Objects/AssemblyInfo.Interface.cs
+++ Zetbox.Objects/AssemblyInfo.Interface.cs
@@ -9,4 +9,4 @@
 [assembly: System.Runtime.InteropServices.ComVisibleAttribute(false)]
 // [assembly: System.CLSCompliantAttribute(true)] // TODO: needs PositionSuffix without underscore and naming columns differently
 [assembly: System.CLSCompliantAttribute(true)]
-[assembly: Zetbox.API.ZetboxGeneratedVersion("cf2b8e6d-105a-4283-83ed-2b92d1648109")]
+[assembly: Zetbox.API.ZetboxGeneratedVersion("8a663720-61d6-4873-a4e3-16c01e8f1055")]
