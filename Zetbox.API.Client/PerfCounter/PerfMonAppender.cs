405362394a27a8c55ad293af9f43fea223091cfb
Arthur Zaczek <arthur@dasz.at>
2019-11-14 21:55:29 +0100

    update Zetbox.API.Server.csproj, not ready yet, need to update arebis

--- Zetbox.API.Client/PerfCounter/PerfMonAppender.cs
+++ Zetbox.API.Client/PerfCounter/PerfMonAppender.cs
@@ -12,6 +12,7 @@
 //
 // You should have received a copy of the GNU Lesser General Public
 // License along with zetbox.  If not, see <http://www.gnu.org/licenses/>.
+#if NETFULL
 namespace Zetbox.API.Client.PerfCounter
 {
     using System;
@@ -120,3 +121,4 @@ namespace Zetbox.API.Client.PerfCounter
 
     }
 }
+#endif
