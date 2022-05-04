368886736317ea064c583a367d2d55e317ba0c9d
Arthur Zaczek <arthur@dasz.at>
2022-05-04 14:29:41 +0200

    Add blazor as Toolkit

--- Zetbox.Objects/AssemblyInfo.Interface.cs
+++ Zetbox.Objects/AssemblyInfo.Interface.cs
@@ -9,4 +9,4 @@
 [assembly: System.Runtime.InteropServices.ComVisibleAttribute(false)]
 // [assembly: System.CLSCompliantAttribute(true)] // TODO: needs PositionSuffix without underscore and naming columns differently
 [assembly: System.CLSCompliantAttribute(true)]
-[assembly: Zetbox.API.ZetboxGeneratedVersion("8ef34b87-ef3d-40df-9089-38d8e2f173bb")]
+[assembly: Zetbox.API.ZetboxGeneratedVersion("5d8451df-bae2-43e6-9a9a-c9060baaf746")]
